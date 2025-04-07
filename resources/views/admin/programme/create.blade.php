a64f7c4121a34646a7db357fe0891d3786b1ef37
nibhashlbsnaa <nibhash.mishra@awzpact.com>
2025-04-07 12:23:05 +0530

    ui new

---
+++ resources/views/admin/programme/create.blade.php
@@ -0,0 +1,76 @@
+@extends('admin.layouts.master')
+
+@section('title', 'Programme - Sargam | Lal Bahadur')
+
+@section('content')
+
+<div class="container-fluid">
+    <div class="card card-body py-3">
+        <div class="row align-items-center">
+            <div class="col-12">
+                <div class="d-sm-flex align-items-center justify-space-between">
+                    <h4 class="mb-4 mb-sm-0 card-title">Create Course</h4>
+                    <nav aria-label="breadcrumb" class="ms-auto">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item d-flex align-items-center">
+                                <a class="text-muted text-decoration-none d-flex" href="index.html">
+                                    <iconify-icon icon="solar:home-2-line-duotone" class="fs-6"></iconify-icon>
+                                </a>
+                            </li>
+                            <li class="breadcrumb-item" aria-current="page">
+                                <span class="badge fw-medium fs-2 bg-primary-subtle text-primary">
+                                    Course
+                                </span>
+                            </li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- start Vertical Steps Example -->
+    <div class="card">
+        <div class="card-body">
+            <h4 class="card-title mb-3">Create Course</h4>
+            <hr>
+            <form>
+                <div class="row">
+                    <div id="course_fields" class="my-2"></div>
+                    <div class="row" id="course_fields">
+                        <div class="col-sm-5">
+                            <label for="Schoolname" class="form-label">School Name :</label>
+                            <div class="mb-3">
+                                <input type="text" class="form-control" id="Schoolname" name="Schoolname"
+                                    placeholder="School Name">
+                            </div>
+                        </div>
+                        <div class="col-sm-5">
+                            <label for="Schoolname" class="form-label">School Name :</label>
+                            <div class="mb-3">
+                                <input type="text" class="form-control" id="Age" name="Age" placeholder="Age">
+                            </div>
+                        </div>
+                        <div class="col-sm-2">
+                            <label for="Schoolname" class="form-label"></label>
+                            <div class="mb-3">
+                                <button onclick="course_fields();" class="btn btn-success fw-medium" type="button">
+                                    <i class="material-icons menu-icon">add</i>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="mb-3">
+                    <button class="btn btn-primary hstack gap-6 float-end" type="submit">
+                    <i class="material-icons menu-icon">send</i>
+                        Submit
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+    <!-- end Vertical Steps Example -->
+</div>
+
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+
+Auth::routes(['verify' => true, 'register' => false]);
+
+Route::get('/', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login'])->name('post_login');
+
+// Route::middleware(['auth', 'verified'])->group(function () {
+    
+//     // Route::name('admin.')->group(function () { // prefix for route name
+        
+//     // });
+// });
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index']);
+
+Route::get('/dashboard', function(){
+    return view('dashboard');
+});
+Route::get('/member-create', function(){
+    return view('admin.member.create');
+});
+Route::get('/member-edit', function(){
+    return view('admin.member.edit');
+});
+Route::get('/member', function(){
+    return view('admin.member.index');
+});
+Route::get('/faculty-create', function(){
+    return view('admin.faculty.create');
+});
+Route::get('/faculty-edit', function(){
+    return view('admin.faculty.edit');
+});
+Route::get('/faculty', function(){
+    return view('admin.faculty.index');
+});
+Route::get('/programme', function(){
+    return view('admin.programme.index');
+});
+Route::get('/programme-create', function(){
+    return view('admin.programme.create');
+});
+Route::get('/programme-edit', function(){
+    return view('admin.programme.edit');
+});
