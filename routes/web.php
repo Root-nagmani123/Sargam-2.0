b73df3376be00b0f34032d8a9a77e684669c4e42
nibhashlbsnaa <nibhash.mishra@awzpact.com>
2025-04-08 12:28:39 +0530

    routes creation

--- routes/web.php
+++ routes/web.php
@@ -64,94 +64,88 @@ Route::middleware(['auth'])->group(function () {
             return view('admin.programme.edit');
         })->name('edit');
     });
-});
 
+    Route::prefix('batch')->name('batch.')->group(function () {
+        Route::get('/', function() {
+            return view('admin.batch.index');
+        })->name('index');
+        
+        Route::get('/create', function() {
+            return view('admin.batch.create');
+        })->name('create');
+        
+        Route::get('/edit', function() {
+            return view('admin.batch.edit');
+        })->name('edit');
+    });
 
+    Route::prefix('subject')->name('subject.')->group(function () {
+        Route::get('/', function() {
+            return view('admin.subject.index');
+        })->name('index');
+        
+        Route::get('/create', function() {
+            return view('admin.subject.create');
+        })->name('create');
+        
+        Route::get('/edit', function() {
+            return view('admin.subject.edit');
+        })->name('edit');
+    });
 
-
-Route::get('/dashboard', function(){
-    return view('dashboard');
-});
-Route::get('/member-create', function(){
-    return view('admin.member.create');
-});
-Route::get('/member-edit', function(){
-    return view('admin.member.edit');
-});
-Route::get('/member', function(){
-    return view('admin.member.index');
-});
-Route::get('/faculty-create', function(){
-    return view('admin.faculty.create');
-});
-Route::get('/faculty-edit', function(){
-    return view('admin.faculty.edit');
-});
-Route::get('/faculty', function(){
-    return view('admin.faculty.index');
-});
-Route::get('/programme', function(){
-    return view('admin.programme.index');
-});
-Route::get('/programme-create', function(){
-    return view('admin.programme.create');
-});
-Route::get('/programme-edit', function(){
-    return view('admin.programme.edit');
-});
-Route::get('/batch', function(){
-    return view('admin.batch.index');
-});
-Route::get('/batch-create', function(){
-    return view('admin.batch.create');
-});
-Route::get('/batch-edit', function(){
-    return view('admin.batch.edit');
-});
-Route::get('/subject', function(){
-    return view('admin.subject.index');
-});
-Route::get('/subject-create', function(){
-    return view('admin.subject.create');
-});
-Route::get('/subject-edit', function(){
-    return view('admin.subject.edit');
-});
-Route::get('/stream', function(){
-    return view('admin.stream.index');
-});
-Route::get('/stream-create', function(){
-    return view('admin.stream.create');
-});
-Route::get('/stream-edit', function(){
-    return view('admin.stream.edit');
-});
-Route::get('/country', function(){
-    return view('admin.country.index');
-});
-Route::get('/country-create', function(){
-    return view('admin.country.create');
-});
-Route::get('/country-edit', function(){
-    return view('admin.country.edit');
-});
-Route::get('/curriculum', function(){
-    return view('admin.curriculum.index');
-});
-Route::get('/curriculum-create', function(){
-    return view('admin.curriculum.create');
-});
-Route::get('/curriculum-edit', function(){
-    return view('admin.curriculum.edit');
-});
-Route::get('/mapping', function(){
-    return view('admin.mapping.index');
-});
-Route::get('/mapping-create', function(){
-    return view('admin.mapping.create');
-});
-Route::get('/mapping-edit', function(){
-    return view('admin.mapping.edit');
+    Route::prefix('stream')->name('stream.')->group(function () {
+        Route::get('/', function() {
+            return view('admin.stream.index');
+        })->name('index');
+        
+        Route::get('/create', function() {
+            return view('admin.stream.create');
+        })->name('create');
+        
+        Route::get('/edit', function() {
+            return view('admin.stream.edit');
+        })->name('edit');
+    });
+    Route::prefix('country')->name('country.')->group(function () {
+        Route::get('/', function() {
+            return view('admin.country.index');
+        })->name('index');
+        
+        Route::get('/create', function() {
+            return view('admin.country.create');
+        })->name('create');
+        
+        Route::get('/edit', function() {
+            return view('admin.country.edit');
+        })->name('edit');
+    });
+    Route::prefix('curriculum')->name('curriculum.')->group(function () {
+        Route::get('/', function() {
+            return view('admin.curriculum.index');
+        })->name('index');
+        
+        Route::get('/create', function() {
+            return view('admin.curriculum.create');
+        })->name('create');
+        
+        Route::get('/edit', function() {
+            return view('admin.curriculum.edit');
+        })->name('edit');
+    });
+    Route::prefix('mapping')->name('mapping.')->group(function () {
+        Route::get('/', function() {
+            return view('admin.mapping.index');
+        })->name('index');
+        
+        Route::get('/create', function() {
+            return view('admin.mapping.create');
+        })->name('create');
+        
+        Route::get('/edit', function() {
+            return view('admin.mapping.edit');
+        })->name('edit');
+    });
+  
 });
 
 
