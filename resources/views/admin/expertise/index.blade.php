302b757bd3e34d31803f4566487103949715d987
nibhashlbsnaa <nibhash.mishra@awzpact.com>
2025-04-16 10:19:24 +0530

    name change

--- resources/views/admin/expertise/index.blade.php
+++ resources/views/admin/expertise/index.blade.php
@@ -27,100 +27,87 @@
             </div>
         </div>
     </div>
-
-    <div class="datatables">
-        <!-- start Zero Configuration -->
-        <div class="card">
-            <div class="card-body">
-                <div class="table-responsive">
-                    <div class="row">
-                        <div class="col-6">
-                            <h4>Area of Expertise</h4>
-                        </div>
-                        <div class="col-6">
-                            <div class="float-end gap-2">
-                                <a href="{{ route('expertise.create') }}" class="btn btn-primary">+ Add Expertise</a>
-                            </div>
-                        </div>
-                    </div>
-                    <hr>
-                    <div id="zero_config_wrapper" class="dataTables_wrapper">
-                        <div class="dataTables_length" id="zero_config_length"><label>Show <select
-                                    name="zero_config_length" aria-controls="zero_config" class="">
-                                    <option value="10">10</option>
-                                    <option value="25">25</option>
-                                    <option value="50">50</option>
-                                    <option value="100">100</option>
-                                </select> entries</label></div>
-                        <div id="zero_config_filter" class="dataTables_filter"><label>Search:<input type="search"
-                                    class="" placeholder="" aria-controls="zero_config"></label></div>
-                        <table id="zero_config"
-                            class="table table-striped table-bordered text-nowrap align-middle dataTable"
-                            aria-describedby="zero_config_info">
-                            <thead>
-                                <!-- start row -->
-                                <tr>
-                                    <th>S.No.</th>
-                                    <th>Area of Expertise</th>
-                                    <th>Action</th>
-                                    <th>Status</th>
-                                </tr>
-                                <!-- end row -->
-                            </thead>
-                            <tbody>
-                                <tr class="odd">
-                                    <td>1</td>
-                                    <td class="sorting_1">
-                                        <div class="d-flex align-items-center gap-6">
-                                            <h6 class="mb-0"> Airi Satou</h6>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div class="d-flex justify-content-start align-items-start gap-2">
-                                            <a href="#" class="btn btn-success text-white btn-sm">
-                                                Edit
-                                            </a>
-                                            <form action="#" method="POST" class="m-0">
-                                                <input type="hidden" name="_token"
-                                                    value="7m53OwU7KaFp1PPyJcyUuVMXW7xvrGr12yL6QycA"> <input
-                                                    type="hidden" name="_method" value="DELETE"> <button type="submit"
-                                                    class="btn btn-danger text-white btn-sm"
-                                                    onclick="return confirm('Are you sure you want to delete?')">
-                                                    Delete
-                                                </button>
-                                            </form>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div class="form-check form-switch">
-                                            <input class="form-check-input status-toggle" type="checkbox" role="switch"
-                                                data-table="news" data-column="status" data-id="21" checked="">
-                                        </div>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <div class="dataTables_info" id="zero_config_info" role="status" aria-live="polite">Showing 1 to
-                            10 of 57 entries</div>
-                        <div class="dataTables_paginate paging_simple_numbers" id="zero_config_paginate"><a
-                                class="paginate_button previous disabled" aria-controls="zero_config"
-                                aria-disabled="true" role="link" data-dt-idx="previous" tabindex="-1"
-                                id="zero_config_previous">Previous</a><span><a class="paginate_button current"
-                                    aria-controls="zero_config" role="link" aria-current="page" data-dt-idx="0"
-                                    tabindex="0">1</a><a class="paginate_button " aria-controls="zero_config"
-                                    role="link" data-dt-idx="1" tabindex="0">2</a><a class="paginate_button "
-                                    aria-controls="zero_config" role="link" data-dt-idx="2" tabindex="0">3</a><a
-                                    class="paginate_button " aria-controls="zero_config" role="link" data-dt-idx="3"
-                                    tabindex="0">4</a><a class="paginate_button " aria-controls="zero_config"
-                                    role="link" data-dt-idx="4" tabindex="0">5</a><a class="paginate_button "
-                                    aria-controls="zero_config" role="link" data-dt-idx="5" tabindex="0">6</a></span><a
-                                class="paginate_button next" aria-controls="zero_config" role="link" data-dt-idx="next"
-                                tabindex="0" id="zero_config_next">Next</a></div>
-                    </div>
+    <div class="widget-content searchable-container list">
+        <div class="card card-body">
+            <div class="row">
+                <div class="col-md-4 col-xl-3">
+                    <form class="position-relative">
+                        <input type="text" class="form-control product-search ps-5" id="input-search">
+                        <i
+                            class="ri-search-line position-absolute top-50 start-0 translate-middle-y fs-6 text-dark ms-3"></i>
+                    </form>
+                </div>
+                <div
+                    class="col-md-8 col-xl-9 text-end d-flex justify-content-md-end justify-content-center mt-3 mt-md-0">
+                    <a href="{{ route('expertise.create') }}" id="btn-add-contact"
+                        class="btn btn-primary d-flex align-items-center">
+                        <i class="ri-add-line text-white me-1 fs-5"></i> Add Expertise
+                    </a>
                 </div>
             </div>
         </div>
-        <!-- end Zero Configuration -->
+        <div class="card card-body">
+            <div class="table-responsive">
+                <table class="table search-table align-middle text-nowrap">
+                    <thead class="header-item">
+                        <tr>
+                            <th>
+                                <div class="n-chk align-self-center text-center">
+                                    <div class="form-check">
+                                        <input type="checkbox" class="form-check-input primary" id="contact-check-all">
+                                        <label class="form-check-label" for="contact-check-all"></label>
+                                        <span class="new-control-indicator"></span>
+                                    </div>
+                                </div>
+                            </th>
+                            <th>Area of Expertise</th>
+                            <th>Action</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <!-- start row -->
+                        <tr class="search-items">
+                            <td>
+                                <div class="n-chk align-self-center text-center">
+                                    <div class="form-check">
+                                        <input type="checkbox" class="form-check-input contact-chkbox primary"
+                                            id="checkbox1">
+                                        <label class="form-check-label" for="checkbox1"></label>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                <div class="d-flex align-items-center">
+                                    <div class="ms-3">
+                                        <div class="user-meta-info">
+                                            <h6 class="user-name mb-0" data-name=" Adams"> Adams</h6>
+                                        </div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                <div class="action-btn">
+                                    <a href="{{ route('expertise.edit', 1) }}" class="text-primary edit">
+                                        <i class="ri-eye-line fs-5"></i>
+                                    </a>
+                                    <a href="javascript:void(0)" class="text-dark delete ms-2">
+                                        <i class="ri-delete-bin-line fs-5"></i>
+                                    </a>
+                                </div>
+                            </td>
+                            <td>
+                                <div class="form-check form-switch">
+                                    <input class="form-check-input status-toggle" type="checkbox" role="switch"
+                                        data-table="news" data-column="status" data-id="21" checked="">
+                                </div>
+                            </td>
+                        </tr>
+                        <!-- end row -->
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 </div>
 
--- resources/views/admin/programme/index.blade.php
+++ resources/views/admin/programme/index.blade.php
@@ -1,6 +1,6 @@
 @extends('admin.layouts.master')
 
-@section('title', 'Programme - Sargam | Lal Bahadur')
+@section('title', 'Course Master - Sargam | Lal Bahadur')
 
 @section('content')
 <div class="container-fluid">
@@ -8,7 +8,7 @@
         <div class="row align-items-center">
             <div class="col-12">
                 <div class="d-sm-flex align-items-center justify-space-between">
-                    <h4 class="mb-4 mb-sm-0 card-title">Programme</h4>
+                    <h4 class="mb-4 mb-sm-0 card-title">Course Master</h4>
                     <nav aria-label="breadcrumb" class="ms-auto">
                         <ol class="breadcrumb">
                             <li class="breadcrumb-item d-flex align-items-center">
@@ -18,7 +18,7 @@
                             </li>
                             <li class="breadcrumb-item" aria-current="page">
                                 <span class="badge fw-medium fs-2 bg-primary-subtle text-primary">
-                                    Programme
+                                    Course Master
                                 </span>
                             </li>
                         </ol>
@@ -35,7 +35,7 @@
                 <div class="table-responsive">
                     <div class="row">
                         <div class="col-6">
-                            <h4>Programme</h4>
+                            <h4>Course Master</h4>
                         </div>
                         <div class="col-6">
                             <div class="float-end gap-2">
