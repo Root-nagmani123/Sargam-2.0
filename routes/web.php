5e82c720d1d26ae59727e6305093ca39819d2540
Mukesh Yadav <mukesh.yadav@awzpact.com>
2025-04-16 15:48:39 +0530

    - fixed

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use App\Http\Controllers\Admin\{
 };
 
 Route::get('clear-cache', function () {
+Route::get('clear-cache', function () {
     Artisan::call('cache:clear');
     Artisan::call('config:clear');
     Artisan::call('view:clear');
