cdcfef49fb712d58b7ebdca89e65a7bbcfaeac50
Root-nagmani123 <nagmani.singh@awzpact.com>
2025-04-23 12:03:35 +0530

    country state city crud code

--- resources/views/admin/country/create.blade.php
+++ resources/views/admin/country/create.blade.php
@@ -4,123 +4,128 @@
 
 @section('content')
 
-<div class="container-fluid">
-    <div class="card card-body py-3">
-        <div class="row align-items-center">
-            <div class="col-12">
-                <div class="d-sm-flex align-items-center justify-space-between">
-                    <h4 class="mb-4 mb-sm-0 card-title">Add Country</h4>
-                    <nav aria-label="breadcrumb" class="ms-auto">
-                        <ol class="breadcrumb">
-                            <li class="breadcrumb-item d-flex align-items-center">
-                                <a class="text-muted text-decoration-none d-flex" href="index.html">
-                                    <iconify-icon icon="solar:home-2-line-duotone" class="fs-6"></iconify-icon>
-                                </a>
-                            </li>
-                            <li class="breadcrumb-item" aria-current="page">
-                                <span class="badge fw-medium fs-2 bg-primary-subtle text-primary">
-                                    Country
-                                </span>
-                            </li>
-                        </ol>
-                    </nav>
+    <div class="container-fluid">
+        <div class="card card-body py-3">
+            <div class="row align-items-center">
+                <div class="col-12">
+                    <div class="d-sm-flex align-items-center justify-space-between">
+                        <h4 class="mb-4 mb-sm-0 card-title">Add Country</h4>
+                        <nav aria-label="breadcrumb" class="ms-auto">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item d-flex align-items-center">
+                                    <a class="text-muted text-decoration-none d-flex" href="index.html">
+                                        <iconify-icon icon="solar:home-2-line-duotone" class="fs-6"></iconify-icon>
+                                    </a>
+                                </li>
+                                <li class="breadcrumb-item" aria-current="page">
+                                    <span class="badge fw-medium fs-2 bg-primary-subtle text-primary">
+                                        Country
+                                    </span>
+                                </li>
+                            </ol>
+                        </nav>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    @if (session('success'))
-    <div class="alert alert-success alert-dismissible fade show" role="alert">
-        {{ session('success') }}
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-    </div>
-    @endif
+        @if (session('success'))
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                {{ session('success') }}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        @endif
 
-    @if (session('error'))
-    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-        {{ session('error') }}
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-    </div>
-    @endif
-    <!-- start Vertical Steps Example -->
-    <div class="card">
-        <div class="card-body">
-            <h4 class="card-title mb-3">Country</h4>
-            <hr>
-            <form action="{{ route('country.store') }}" method="POST">
-                @csrf
-                <div class="card">
-                    <div class="card-body">
-                        <h4 class="card-title mb-3">Country</h4>
-                        <hr>
-                        <div class="row" id="country_fields">
-                            <div class="row">
-                                <div class="col-sm-10">
-                                    <label class="form-label">Country Name :</label>
-                                    <div class="mb-3">
-                                        <input type="text" class="form-control" name="country_name[]"
-                                            placeholder="Country Name" value="{{ old('country_name.0') }}">
-                                        @error('country_name.0')
-                                        <p class="text-danger">{{ $message }}</p>
-                                        @enderror
+        @if (session('error'))
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                {{ session('error') }}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        @endif
+        <!-- start Vertical Steps Example -->
+        <div class="card">
+            <div class="card-body">
+                <h4 class="card-title mb-3">Country</h4>
+                <hr>
+                <form action="{{ route('country.store') }}" method="POST">
+                    @csrf
+                    <div class="card">
+                        <div class="card-body">
+                            <h4 class="card-title mb-3">Country</h4>
+                            <hr>
+                            <div class="row" id="country_fields">
+                                <div class="row">
+                                    <div class="col-sm-10">
+                                        <label class="form-label">Country Name :</label>
+                                        <div class="mb-3">
+                                            <input type="text" class="form-control" name="country_name[]"
+                                                placeholder="Country Name" value="{{ old('country_name.0') }}">
+                                            @error('country_name.0')
+                                                <p class="text-danger">{{ $message }}</p>
+                                            @enderror
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="col-sm-2">
-                                    <label class="form-label">&nbsp;</label>
-                                    <div class="mb-3">
-                                        <button onclick="addCountryField();" class="btn btn-success fw-medium"
-                                            type="button">
-                                            <i class="material-icons menu-icon">add</i>
-                                        </button>
+                                    <div class="col-sm-2">
+                                        <label class="form-label">&nbsp;</label>
+                                        <div class="mb-3">
+                                            <button onclick="addCountryField();" class="btn btn-success fw-medium"
+                                                type="button">
+                                                <i class="material-icons menu-icon">add</i>
+                                            </button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <hr>
-                        <div class="mb-3">
-                            <button class="btn btn-primary hstack gap-6 float-end" type="submit">
-                                <i class="material-icons menu-icon">send</i> Submit
-                            </button>
+                            <hr>
+                            <div class="mb-3">
+                                <button class="btn btn-primary hstack gap-6 float-end" type="submit">
+                                    <i class="material-icons menu-icon">send</i> Submit
+                                </button>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </form>
+                </form>
 
 
-            <script>
-             function addCountryField() {
-        var newField = `
-            <div class="row">
-                <div class="col-sm-10">
-                    <label class="form-label">Country Name :</label>
-                    <div class="mb-3">
-                        <input type="text" class="form-control" name="country_name[]" placeholder="Country Name">
-                        @error('country_name.*')
-                            <p class="text-danger">{{ $message }}</p>
-                        @enderror
-                    </div>
-                </div>
-                <div class="col-sm-2">
-                    <label class="form-label">&nbsp;</label>
-                    <div class="mb-3">
-                        <button onclick="removeCountryField(this);" class="btn btn-danger fw-medium" type="button">
-                            <i class="material-icons menu-icon">remove</i>
-                        </button>
-                    </div>
-                </div>
+
+
             </div>
-        `;
+        </div>
+        <!-- end Vertical Steps Example -->
+    </div>
+
+
+@endsection
+
+@section('scripts')
+<script>
+    function addCountryField() {
+            var newField = `
+    <div class="row">
+        <div class="col-sm-10">
+           <label class="form-label">Country Name :</label>
+           <div class="mb-3">
+               <input type="text" class="form-control" name="country_name[]" placeholder="Country Name">
+               @error('country_name.*')
+                   <p class="text-danger">{{ $message }}</p>
+               @enderror
+           </div>
+       </div>
+       <div class="col-sm-2">
+           <label class="form-label">&nbsp;</label>
+           <div class="mb-3">
+               <button onclick="removeCountryField(this);" class="btn btn-danger fw-medium" type="button">
+                   <i class="material-icons menu-icon">remove</i>
+               </button>
+           </div>
+       </div>
+   </div>
+`;
         document.getElementById('country_fields').insertAdjacentHTML('beforeend', newField);
     }
 
     function removeCountryField(button) {
         button.closest('.row').remove();
     }
-            </script>
+</script>
 
-        </div>
-    </div>
-    <!-- end Vertical Steps Example -->
-</div>
-
-
-@endsection
+@ensection
