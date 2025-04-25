30061b57e6fb6992459ced1248b3e5f0aeff9656
Mukesh Yadav <mukesh.yadav@awzpact.com>
2025-04-25 14:58:00 +0530

    Faculty Expertise Master

---
+++ app/Models/FacultyExpertiseMaster.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class FacultyExpertiseMaster extends Model
+{
+    protected $table = 'faculty_expertise_master';
+    protected $primaryKey = 'pk';
+    protected $guarded = [];
+    public $timestamps = false;
+}
