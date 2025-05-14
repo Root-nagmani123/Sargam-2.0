4671207c8e9e07f05dd7bd8d1a7b4f236508384d
Root-nagmani123 <nagmani.singh@awzpact.com>
2025-05-14 11:44:17 +0530

    calendr work

---
+++ app/Models/CalendarEvent.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CalendarEvent extends Model
+{
+    protected $table = 'timetable';
+public $timestamps = false; 
+protected $fillable = [
+    'course_master_pk',
+    'subject_master_pk',
+    'subject_module_master_pk',
+    'subject_topic',
+    'course_group_type_master',
+    'group_name',
+    'faculty_master',
+    'faculty_type',
+    'venue_id',
+    'class_session_master_pk',
+    'fullday',
+    'mannual_starttime',
+    'mannual_end_time',
+    'feedback_checkbox',
+    'ratting_checkbox',
+    'remark_checkbox',
+    'bio_attendance',
+    'active_inactive',
+    'created_date',
+    'modified_date',
+];
+}
