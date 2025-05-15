5a0c8a0cc063671931c99666fe472b627bb821c9
nibhashlbsnaa <nibhash.mishra@awzpact.com>
2025-05-15 09:13:58 +0530

    routesfor forms

--- resources/views/admin/forms/course_list.blade.php
+++ resources/views/admin/forms/course_list.blade.php
@@ -2,67 +2,71 @@
 @section('title', 'Course List')
 
 @section('content')
-    <div class="container-fluid mt-4">
-        <h4>Total Registered Students: {{ $total_students }}</h4>
-        <hr>
+<div class="container-fluid mt-4">
+    <div class="card">
+        <div class="card-body">
+            <h4>Total Registered Students: {{ $total_students }}</h4>
+            <hr>
 
-        <form method="GET" class="mb-3">
-            <label>Status Filter:</label>
-            <select name="statusval" onchange="this.form.submit()" class="form-select w-auto d-inline-block ms-2">
-                <option value="">All</option>
-                <option value="1" {{ $statusval == 1 ? 'selected' : '' }}>confirm</option>
-                <option value="2" {{ $statusval == 2 ? 'selected' : '' }}>Not confirm</option>
-            </select>
-        </form>
+            <form method="GET" class="mb-3">
+                <label>Status Filter:</label>
+                <select name="statusval" onchange="this.form.submit()" class="form-select w-auto d-inline-block ms-2">
+                    <option value="">All</option>
+                    <option value="1" {{ $statusval == 1 ? 'selected' : '' }}>confirm</option>
+                    <option value="2" {{ $statusval == 2 ? 'selected' : '' }}>Not confirm</option>
+                </select>
+            </form>
 
-        </a>
+            </a>
 
-        <table class="table table-striped table-bordered text-nowrap">
-            <thead>
-                <tr>
-                    <th>S.No</th>
-                    @foreach ($fields as $field)
-                        <th>{{ ucfirst($field) }}</th>
-                    @endforeach
-                    <th>Download PDF</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($records as $record)
+            <table class="table table-striped table-bordered table-responsive">
+                <thead>
+                    <tr>
+                        <th class="col">S.No</th>
+                        @foreach ($fields as $field)
+                        <th class="col">{{ ucfirst($field) }}</th>
+                        @endforeach
+                        <th class="col">Download PDF</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($records as $record)
                     @php $uid = $record->uid; @endphp
                     @if (isset($users[$uid]))
-                        <tr>
-                            <td>{{ $loop->iteration }}</td> {{-- S.No column --}}
+                    <tr>
+                        <td>{{ $loop->iteration }}</td> {{-- S.No column --}}
 
-                            @foreach ($fields as $field)
-                                <td>
-                                    @php
-                                        $value = $users[$uid][$field] ?? '';
-                                        $extension = pathinfo($value, PATHINFO_EXTENSION);
-                                    @endphp
+                        @foreach ($fields as $field)
+                        <td>
+                            @php
+                            $value = $users[$uid][$field] ?? '';
+                            $extension = pathinfo($value, PATHINFO_EXTENSION);
+                            @endphp
 
-                                    @if (in_array(strtolower($extension), ['jpg', 'jpeg', 'png', 'gif']))
-                                        <img src="{{ asset('uploads/' . $value) }}" width="100" alt="Image">
-                                    @elseif (strtolower($extension) === 'pdf')
-                                        <a href="{{ asset('uploads/' . $value) }}" target="_blank">View PDF</a>
-                                    @else
-                                        {{ $value }}
-                                    @endif
-                                </td>
-                            @endforeach
-                            <td>
-                                <a
-                                    href="{{ route('forms.downloadpdf', ['formid' => $formid, 'uid' => $uid]) }}">Download</a>
-                            </td>
-                            {{-- <td>{{ $record->confirm_status }}</td> --}}
-                        </tr>
+                            @if (in_array(strtolower($extension), ['jpg', 'jpeg', 'png', 'gif']))
+                            <img src="{{ asset('uploads/' . $value) }}" width="100" alt="Image">
+                            @elseif (strtolower($extension) === 'pdf')
+                            <a href="{{ asset('uploads/' . $value) }}" target="_blank">View PDF</a>
+                            @else
+                            {{ $value }}
+                            @endif
+                        </td>
+                        @endforeach
+                        <td>
+                            <a
+                                href="{{ route('forms.downloadpdf', ['formid' => $formid, 'uid' => $uid]) }}">Download</a>
+                        </td>
+                        {{-- <td>{{ $record->confirm_status }}</td> --}}
+                    </tr>
                     @endif
-                @endforeach
-            </tbody>
-        </table>
+                    @endforeach
+                </tbody>
+            </table>
 
-        <div class="d-flex justify-content-center">
-            {{-- {{ $records->appends(['statusval' => $statusval])->links() }} --}}
+            <div class="d-flex justify-content-center">
+                {{-- {{ $records->appends(['statusval' => $statusval])->links() }} --}}
+            </div>
         </div>
     </div>
+</div>
 @endsection
