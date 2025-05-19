9bdfbde91c5370c9870e51e9f85d11df4d2530ee
nibhashlbsnaa <nibhash.mishra@awzpact.com>
2025-05-19 10:56:16 +0530

    group mapping

--- resources/views/admin/group_mapping/create.blade.php
+++ resources/views/admin/group_mapping/create.blade.php
@@ -16,64 +16,40 @@
             <hr>
             <form action="{{ route('group.mapping.store') }}" method="POST" id="classSessionForm">
                 @csrf
-                @if(!empty($groupMapping)) 
-                    <input type="hidden" name="id" value="{{ encrypt($groupMapping->pk) }}">
+                @if(!empty($groupMapping))
+                <input type="hidden" name="id" value="{{ encrypt($groupMapping->pk) }}">
                 @endif
                 <div class="row">
-                    
-                    
-
-                    <div class="col-md-6">
+                    <div class="col-6">
                         <div class="mb-3">
-                            <x-select 
-                                name="course_id" 
-                                label="Course name :" 
-                                placeholder="Course name" 
-                                formLabelClass="form-label"
-                                formSelectClass="select2"
-                                required="true"
-                                :options="$courses" 
-                                :value="old('course_id', $groupMapping->course_name ?? '')"
-                                />
+                            <x-select name="course_id" label="Course name :" placeholder="Course name"
+                                formLabelClass="form-label" formSelectClass="select2" required="true"
+                                :options="$courses" :value="old('course_id', $groupMapping->course_name ?? '')" />
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="mb-3">
+                            <x-select name="type_id" label="Group Type :" placeholder="Group Type"
+                                formLabelClass="form-label" formSelectClass="select2" required="true"
+                                :options="$courseGroupTypeMaster"
+                                :value="old('type_name', $groupMapping->type_name ?? '')" />
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="mb-3">
+                            <x-input name="group_name" label="Group Name :" placeholder="Group Name"
+                                formLabelClass="form-label" required="true"
+                                value="{{ old('group_name', $groupMapping->group_name ?? '') }}" />
                         </div>
                     </div>
 
-                    <div class="col-md-6">
-                        <div class="mb-3">
-                            <x-select 
-                                name="type_id" 
-                                label="Group Type :" 
-                                placeholder="Group Type" 
-                                formLabelClass="form-label"
-                                formSelectClass="select2"
-                                required="true"
-                                :options="$courseGroupTypeMaster" 
-                                :value="old('type_name', $groupMapping->type_name ?? '')"
-                                />
-                        </div>
-                    </div>
-
-                    <div class="col-md-6">
-                        <div class="mb-3">
-                            <x-input 
-                                name="group_name" 
-                                label="Group Name :" 
-                                placeholder="Group Name" 
-                                formLabelClass="form-label"
-                                required="true"
-                                value="{{ old('group_name', $groupMapping->group_name ?? '') }}"
-                                />
-                        </div>
-                    </div>
-                    
                 </div>
-                <div class="mb-3">
-                    <button class="btn btn-primary hstack gap-6 float-end" type="submit" id="saveClassSessionForm">
-                        <i class="material-icons menu-icon">save</i>
+                <hr>
+                <div class="mb-3 gap-2 float-end">
+                    <button class="btn btn-primary " type="submit" id="saveClassSessionForm">
                         Save
                     </button>
-                    <a href="{{ route('group.mapping.index') }}" class="btn btn-secondary hstack gap-6 float-end me-2">
-                        <i class="material-icons menu-icon">arrow_back</i>
+                    <a href="{{ route('group.mapping.index') }}" class="btn btn-secondary">
                         Back
                     </a>
                 </div>
