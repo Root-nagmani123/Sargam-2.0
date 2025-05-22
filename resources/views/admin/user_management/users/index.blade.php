0873b42d20bc8369cea2773b138252c91ec4770e
nibhashlbsnaa <nibhash.mishra@awzpact.com>
2025-05-22 11:58:23 +0530

    edit page changes

--- resources/views/admin/user_management/users/index.blade.php
+++ resources/views/admin/user_management/users/index.blade.php
@@ -25,15 +25,6 @@
                     
                     <hr>
                     <div id="zero_config_wrapper" class="dataTables_wrapper">
-                        <div class="dataTables_length" id="zero_config_length"><label>Show <select
-                                    name="zero_config_length" aria-controls="zero_config" class="">
-                                    <option value="10">10</option>
-                                    <option value="25">25</option>
-                                    <option value="50">50</option>
-                                    <option value="100">100</option>
-                                </select> entries</label></div>
-                        <div id="zero_config_filter" class="dataTables_filter"><label>Search:<input type="search"
-                                    class="" placeholder="" aria-controls="zero_config"></label></div>
                         <table id="zero_config"
                             class="table table-striped table-bordered text-nowrap align-middle dataTable"
                             aria-describedby="zero_config_info">
@@ -94,22 +85,6 @@
                                 @endif
                             </tbody>
                         </table>
-                        <div class="dataTables_info" id="zero_config_info" role="status" aria-live="polite">Showing 1 to
-                            {{ $users->count() }} of {{ $users->total() }} entries</div>
-                        <div class="dataTables_paginate paging_simple_numbers" id="zero_config_paginate"><a
-                                class="paginate_button previous disabled" aria-controls="zero_config"
-                                aria-disabled="true" role="link" data-dt-idx="previous" tabindex="-1"
-                                id="zero_config_previous">Previous</a><span><a class="paginate_button current"
-                                    aria-controls="zero_config" role="link" aria-current="page" data-dt-idx="0"
-                                    tabindex="0">1</a><a class="paginate_button " aria-controls="zero_config"
-                                    role="link" data-dt-idx="1" tabindex="0">2</a><a class="paginate_button "
-                                    aria-controls="zero_config" role="link" data-dt-idx="2" tabindex="0">3</a><a
-                                    class="paginate_button " aria-controls="zero_config" role="link" data-dt-idx="3"
-                                    tabindex="0">4</a><a class="paginate_button " aria-controls="zero_config"
-                                    role="link" data-dt-idx="4" tabindex="0">5</a><a class="paginate_button "
-                                    aria-controls="zero_config" role="link" data-dt-idx="5" tabindex="0">6</a></span><a
-                                class="paginate_button next" aria-controls="zero_config" role="link" data-dt-idx="next"
-                                tabindex="0" id="zero_config_next">Next</a></div>
                     </div>
                 </div>
             </div>
