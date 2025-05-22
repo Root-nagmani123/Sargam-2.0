c76a51b4f880c91a45b0e5adb2c800d9a954b9ef
Root-nagmani123 <nagmani.singh@awzpact.com>
2025-05-22 10:18:42 +0530

    timetable bug solve

--- resources/views/admin/feedback/student_feedback.blade.php
+++ resources/views/admin/feedback/student_feedback.blade.php
@@ -64,11 +64,19 @@
                         <input type="hidden" name="topic_name[]" value="{{ $feedback->subject_topic }}">
 <<<<<<< HEAD
 <<<<<<< HEAD
+=======
+=======
+>>>>>>> 1f00172 (timetable bug solve)
+<<<<<<< HEAD
 
                         <div class="col-md-6">
 =======
+<<<<<<< HEAD
 =======
 >>>>>>> d707d11 (timetable bug solve)
+=======
+>>>>>>> cb8cad4 (timetable bug solve)
+>>>>>>> 1f00172 (timetable bug solve)
                        <input type="hidden" name="Remark_checkbox[]" value="{{ $feedback->Remark_checkbox }}">
                     <input type="hidden" name="Ratting_checkbox[]" value="{{ $feedback->Ratting_checkbox }}">
                         <div class="col-12 col-md-12">
@@ -77,9 +85,15 @@
                         </div>
                         <div class="col-md-4">
 <<<<<<< HEAD
+<<<<<<< HEAD
 >>>>>>> d707d11 (timetable bug solve)
 =======
 >>>>>>> d707d11 (timetable bug solve)
+=======
+=======
+>>>>>>> d707d11 (timetable bug solve)
+>>>>>>> cb8cad4 (timetable bug solve)
+>>>>>>> 1f00172 (timetable bug solve)
                             <label>Course</label>
                             <input type="text" class="form-control" value="{{ $feedback->course_name }}" readonly>
                         </div>
