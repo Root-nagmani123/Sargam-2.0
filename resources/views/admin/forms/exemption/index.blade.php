5f1c8dccb49218bb219ac4573677d21dc6ebaea6
nibhashlbsnaa <nibhash.mishra@awzpact.com>
2025-05-29 10:42:31 +0530

    feedback exemption

--- resources/views/admin/forms/exemption/index.blade.php
+++ resources/views/admin/forms/exemption/index.blade.php
@@ -3,63 +3,69 @@
 @section('title', 'Exemption Master - Sargam | Lal Bahadur')
 
 @section('content')
-    <div class="container-fluid">
-        <div class="card card-body py-3">
-            <div class="row align-items-center">
-                <div class="col-12">
-                    <div class="d-sm-flex align-items-center justify-space-between">
-                        <h4 class="mb-4 mb-sm-0 card-title">Exemption Master</h4>
-                        <nav aria-label="breadcrumb" class="ms-auto">
-                            <ol class="breadcrumb">
-                                <li class="breadcrumb-item" aria-current="page">
-                                    <span class="badge fw-medium fs-2 bg-primary-subtle text-primary">
-                                        Exemption Master
-                                    </span>
-                                </li>
-                            </ol>
-                        </nav>
-                    </div>
+<div class="container-fluid">
+    <div class="card card-body py-3">
+        <div class="row align-items-center">
+            <div class="col-12">
+                <div class="d-sm-flex align-items-center justify-space-between">
+                    <h4 class="mb-4 mb-sm-0 card-title">Exemption Master</h4>
+                    <nav aria-label="breadcrumb" class="ms-auto">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item" aria-current="page">
+                                <span class="badge fw-medium fs-2 bg-primary-subtle text-primary">
+                                    Exemption Master
+                                </span>
+                            </li>
+                        </ol>
+                    </nav>
                 </div>
             </div>
         </div>
+    </div>
 
-        <div class="datatables">
-            <!-- start Table -->
-            <div class="card">
-                <div class="card-body">
-                    <div class="table-responsive">
-                        <div class="row mb-3">
-                            <div class="col-6">
-                                <h4>Exemptions</h4>
-                            </div>
-                            <div class="col-6 text-end">
-                                <a href="{{ route('admin.fc_exemption.create') }}" class="btn btn-primary">+ Add
-                                    Exemption</a>
-                            </div>
+    <div class="datatables">
+        <!-- start Table -->
+        <div class="card">
+            <div class="card-body">
+                <div class="table-responsive">
+                    <div class="row mb-3">
+                        <div class="col-6">
+                            <h4>Exemptions</h4>
                         </div>
-                        <hr>
-                        <table class="table table-striped table-bordered">
+                        <div class="col-6 text-end">
+                            <a href="{{ route('admin.fc_exemption.create') }}" class="btn btn-primary">+ Add
+                                Exemption</a>
+                        </div>
+                    </div>
+                    <hr>
+                    <div id="zero_config_wrapper" class="dataTables_wrapper">
+                        <div class="table-responsive">
+                            <table id="zero_config"
+                            class="table table-striped table-bordered align-middle dataTable"
+                            aria-describedby="zero_config_info">
                             <thead>
                                 <tr>
-                                    <th>S.No</th>
-                                    <th>Exemption Name</th>
-                                    <th>Short Name</th>
-                                    <th>Created Date</th>
-                                    <th>Created By</th>
-                                    <th>Modified By</th>
-                                    <th>Action</th>
+                                    <th class="col">S.No</th>
+                                    <th class="col">Exemption Name</th>
+                                    <th class="col">Short Name</th>
+                                    <th class="col">Created Date</th>
+                                    <th class="col">Created By</th>
+                                    <th class="col">Modified By</th>
+                                    <th class="col">Action</th>
+                                    <th class="col">Status</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 @forelse ($exemptions as $index => $exemption)
-                                    <tr>
-                                        <td>{{ $index + 1 }}</td>
-                                        <td>{{ $exemption->Exemption_name }}</td>
-                                        <td>{{ $exemption->Exemption_short_name }}</td>
-                                        <td>{{ $exemption->Created_date }}</td>
-                                        <td>{{ $exemption->createdByUser->name ?? 'N/A' }}</td>
-                                        <td>{{ $exemption->modifiedByUser->name ?? 'N/A' }}</td>
-                                        <td>
+                                <tr>
+                                    <td>{{ $index + 1 }}</td>
+                                    <td>{{ $exemption->Exemption_name }}</td>
+                                    <td>{{ $exemption->Exemption_short_name }}</td>
+                                    <td>{{ $exemption->Created_date }}</td>
+                                    <td>{{ $exemption->createdByUser->name ?? 'N/A' }}</td>
+                                    <td>{{ $exemption->modifiedByUser->name ?? 'N/A' }}</td>
+                                    <td>
+                                        <div class="d-flex gap-2">
                                             <a href="{{ route('admin.fc_exemption.edit', $exemption->Pk) }}"
                                                 class="btn btn-sm btn-info">
                                                 Edit
@@ -74,27 +80,34 @@
                                                     Delete
                                                 </button>
                                             </form>
-                                            <!-- Toggle Button for visible -->
-                                            <div class="form-check form-switch d-inline-block">
-                                                <input class="form-check-input status-toggle" type="checkbox" role="switch"
-                                                    data-table="fc_exemption_master" data-column="visible"
-                                                    data-id="{{ $exemption->Pk }}"
-                                                    {{ $exemption->visible == 1 ? 'checked' : '' }}>
-                                            </div>
-                                        </td>
-                                    </tr>
+                                        </div>
+
+                                    </td>
+                                    <td>
+                                        <!-- Toggle Button for visible -->
+                                        <div class="form-check form-switch d-inline-block">
+                                            <input class="form-check-input status-toggle" type="checkbox" role="switch"
+                                                data-table="fc_exemption_master" data-column="visible"
+                                                data-id="{{ $exemption->Pk }}"
+                                                {{ $exemption->visible == 1 ? 'checked' : '' }}>
+                                        </div>
+                                    </td>
+                                </tr>
                                 @empty
-                                    <tr>
-                                        <td colspan="7" class="text-center">No exemptions found.</td>
-                                    </tr>
+                                <tr>
+                                    <td colspan="8" class="text-center">No exemptions found.</td>
+                                </tr>
                                 @endforelse
                             </tbody>
                         </table>
+                        </div>
                     </div>
+
                 </div>
             </div>
-            <!-- end Table -->
         </div>
+        <!-- end Table -->
     </div>
+</div>
 
 @endsection
