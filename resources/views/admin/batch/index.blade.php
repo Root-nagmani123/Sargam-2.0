43c4960649933a61713ef370a51c25f63d522724
nibhashlbsnaa <nibhash.mishra@awzpact.com>
2025-05-29 10:42:31 +0530

    feedback exemption

--- resources/views/admin/batch/index.blade.php
+++ resources/views/admin/batch/index.blade.php
@@ -45,92 +45,26 @@
                     </div>
                     <hr>
                     <div id="zero_config_wrapper" class="dataTables_wrapper">
-                        <div class="dataTables_length" id="zero_config_length"><label>Show <select
-                                    name="zero_config_length" aria-controls="zero_config" class="">
-                                    <option value="10">10</option>
-                                    <option value="25">25</option>
-                                    <option value="50">50</option>
-                                    <option value="100">100</option>
-                                </select> entries</label></div>
-                        <div id="zero_config_filter" class="dataTables_filter"><label>Search:<input type="search"
-                                    class="" placeholder="" aria-controls="zero_config"></label></div>
                         <table id="zero_config"
                             class="table table-striped table-bordered text-nowrap align-middle dataTable"
                             aria-describedby="zero_config_info">
                             <thead>
                                 <!-- start row -->
                                 <tr>
-                                    <th class="sorting sorting_asc" tabindex="0" aria-controls="zero_config" rowspan="1"
-                                        colspan="1" aria-sort="ascending"
-                                        aria-label="Name: activate to sort column descending" style="width: 224.625px;">
-                                        S.No.</th>
-                                    <th class="sorting" tabindex="0" aria-controls="zero_config" rowspan="1" colspan="1"
-                                        aria-label="Position: activate to sort column ascending"
-                                        style="width: 225.875px;">Batch Name</th>
-                                    <th class="sorting" tabindex="0" aria-controls="zero_config" rowspan="1" colspan="1"
-                                        aria-label="Office: activate to sort column ascending"
-                                        style="width: 106.453px;">Batch Short Name</th>
-                                    <th class="sorting" tabindex="0" aria-controls="zero_config" rowspan="1" colspan="1"
-                                        aria-label="Salary: activate to sort column ascending"
-                                        style="width: 85.8906px;">Action</th>
-                                    <th class="sorting" tabindex="0" aria-controls="zero_config" rowspan="1" colspan="1"
-                                        aria-label="Salary: activate to sort column ascending"
-                                        style="width: 85.8906px;">Status</th>
+                                    <th class="col">S.No.</th>
+                                    <th class="col">Batch Name</th>
+                                    <th class="col">Batch Short Name</th>
+                                    <th class="col">Action</th>
+                                    <th class="col">Status</th>
                                 </tr>
                                 <!-- end row -->
                             </thead>
                             <tbody>
                                 <tr class="odd">
-                                    <td>1</td>
-                                    <td class="sorting_1">
-                                        <div class="d-flex align-items-center gap-6">
-                                            <img src="../assets/images/profile/user-5.jpg" width="45"
-                                                class="rounded-circle">
-                                            <h6 class="mb-0"> Airi Satou</h6>
-                                        </div>
-                                    </td>
-                                    <td>Accountant</td>
-                                    <td>
-                                        <div class="d-flex justify-content-start align-items-start gap-2">
-                                            <a href="#" class="btn btn-success text-white btn-sm">
-                                                Edit
-                                            </a>
-                                            <form action="#" method="POST" class="m-0">
-                                                <input type="hidden" name="_token"
-                                                    value="7m53OwU7KaFp1PPyJcyUuVMXW7xvrGr12yL6QycA"> <input
-                                                    type="hidden" name="_method" value="DELETE"> <button type="submit"
-                                                    class="btn btn-danger text-white btn-sm"
-                                                    onclick="return confirm('Are you sure you want to delete?')">
-                                                    Delete
-                                                </button>
-                                            </form>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div class="form-check form-switch">
-                                            <input class="form-check-input status-toggle" type="checkbox" role="switch"
-                                                data-table="news" data-column="status" data-id="21" checked="">
-                                        </div>
-                                    </td>
+                                    
                                 </tr>
                             </tbody>
                         </table>
-                        <div class="dataTables_info" id="zero_config_info" role="status" aria-live="polite">Showing 1 to
-                            10 of 57 entries</div>
-                        <div class="dataTables_paginate paging_simple_numbers" id="zero_config_paginate"><a
-                                class="paginate_button previous disabled" aria-controls="zero_config"
-                                aria-disabled="true" role="link" data-dt-idx="previous" tabindex="-1"
-                                id="zero_config_previous">Previous</a><span><a class="paginate_button current"
-                                    aria-controls="zero_config" role="link" aria-current="page" data-dt-idx="0"
-                                    tabindex="0">1</a><a class="paginate_button " aria-controls="zero_config"
-                                    role="link" data-dt-idx="1" tabindex="0">2</a><a class="paginate_button "
-                                    aria-controls="zero_config" role="link" data-dt-idx="2" tabindex="0">3</a><a
-                                    class="paginate_button " aria-controls="zero_config" role="link" data-dt-idx="3"
-                                    tabindex="0">4</a><a class="paginate_button " aria-controls="zero_config"
-                                    role="link" data-dt-idx="4" tabindex="0">5</a><a class="paginate_button "
-                                    aria-controls="zero_config" role="link" data-dt-idx="5" tabindex="0">6</a></span><a
-                                class="paginate_button next" aria-controls="zero_config" role="link" data-dt-idx="next"
-                                tabindex="0" id="zero_config_next">Next</a></div>
                     </div>
                 </div>
             </div>
