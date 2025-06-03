87414e8f6ffb61c449aa0344688f0719df8ed115
nibhashlbsnaa <nibhash.mishra@awzpact.com>
2025-06-03 11:32:12 +0530

    new ui for memo

--- resources/views/admin/master/memo/create.blade.php
+++ resources/views/admin/master/memo/create.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'Create Memo Management - Sargam | Lal Bahadur Shastri National Academy of Administration')
 
 @section('content')
+<link rel="stylesheet" href="{{asset('admin_assets/css/dual-listbox.css')}}">
 <div class="container-fluid">
     <x-breadcrum title="Create Memo/Notice Management" />
     <x-session_message />
@@ -55,7 +56,47 @@
                                 <!-- Options will be populated dynamically -->
                         </div>
                     </div>
-                    
+                    <div class="col-6">
+                        <div class="mb-3">
+                            <label for="" class="form-label">Session</label>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="mb-3">
+                            <label for="" class="form-label">Faculty Name</label>
+                            <select name="" class="form-control" id="">
+                                <option value="">Select Faculty</option>
+                                <!-- Options will be populated dynamically -->
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="mb-3">
+                            <label for="" class="form-label">Faculty Name</label>
+                            <select name="" class="form-control" id="">
+                                <option value="">Select Faculty</option>
+                                <!-- Options will be populated dynamically -->
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <label for="selected_student_list" class="form-label">Select Students</label>
+                        <select id="select" class="select1 form-control" name="selected_student_list[]" multiple>
+
+                        </select>
+                        @error('selected_student_list')
+                        <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+                    <div class="col-12">
+                        <label for="textarea" class="form-label">Message (If Any) </label>
+                        <textarea class="form-control" id="textarea" rows="3" placeholder="Enter remarks..."
+                            name="Remark"></textarea>
+                        @error('Remark')
+                        <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+
 
                 </div>
 
@@ -79,6 +120,58 @@
 
         </div>
     </div>
+    <div class="bg-white p-4 rounded shadow-sm">
+    <h5 class="text-center fw-bold mb-3">88th Foundation Course</h5>
+    <p class="text-center mb-0">Lal Bahadur Shastri National Academy of Administration, Mussoorie</p>
+    <hr>
+
+    <p class="mb-1">SHOW CAUSE NOTICE</p>
+    <p><strong>Date:</strong> 22/11/2013</p>
+
+    <p>It has been brought to the notice of the undersigned that you were absent without prior authorization from
+        following session(s)...</p>
+
+    <div class="table-responsive mb-3">
+        <table class="table table-bordered text-center">
+            <thead class="table-light">
+                <tr>
+                    <th>Date</th>
+                    <th>No. of Session(s)</th>
+                    <th>Topics</th>
+                    <th>
+                        Venue
+                    </th>
+                    <th>Session(s)</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>22-11-2013</td>
+                    <td>1</td>
+                    <td>Lorem ipsum dolor sit amet.</td>
+                    <td>Lorem, ipsum.</td>
+                    <td>06:00-07:00</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <div class="mb-4">
+        <p class="fw-bold">You are advised to do the following:</p>
+        <ul>
+            <li>Reply to this Memo online through this <a href="#">conversation</a></li>
+            <li>Appear <a href="#">in person before the undersigned at 1800 hrs on next working day</a></li>
+        </ul>
+        <p>In absence of online explanation and your personal appearance, unilateral decision may be taken.</p>
+    </div>
+
+    <p><strong>ALBY VARGHESE, A42</strong><br>
+        Remarks: Show Cause Notice for 22.11.13</p>
+
+    <p class="text-end"><strong>Rajesh Arya</strong><br>Deputy Director Sr. & I/C Discipline 88th F.C.</p>
+
+</div>
     <!-- end Vertical Steps Example -->
 </div>
+
 @endsection
