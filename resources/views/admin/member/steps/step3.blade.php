18aa28d3e1a04fce7aa12799317f4d421f3fe92d
Mukesh Yadav <mukesh.yadav@awzpact.com>
2025-06-03 15:26:53 +0530

    Changes

--- resources/views/admin/member/steps/step3.blade.php
+++ resources/views/admin/member/steps/step3.blade.php
@@ -3,33 +3,31 @@
         <div class="mb-3">
 
             @php
-            $userRoleOptions = [
-
-            ];
+                $userRoleOptions = [
+                    
+                ];
             @endphp
 
-            <x-select name="userrole" label="User Role :" :options="$userRoleOptions" :value="old('userrole')"
-                formLabelClass="form-label" formSelectClass="form-select" />
+            <x-select name="userrole" label="User Role :" :options="$userRoleOptions" :value="old('userrole')" formLabelClass="form-label" formSelectClass="form-select" />
 
-
+            
         </div>
     </div> --}}
     @php
         $userRoleOptions = App\Models\UserRoleMaster::getUserRoleList();
-    @endphp
 
+    @endphp
     <div class="col-md-12">
         <div class="mb-3">
-            <label class="form-label fw-bold fs-5" for="role">Role Options:</label>
+            <label class="form-label" for="role">Role Options :</label>
             <div class="controls">
-                <div class="row g-2">
+                <div class="row">
                     @foreach ($userRoleOptions as $key => $value)
                         <div class="col-md-4">
-                            <input type="checkbox" class="btn-check" id="userrole{{ $key }}" name="userrole[]"
-                                value="{{ $key }}" {{ in_array($key, old('userrole', [])) ? 'checked' : '' }}>
-                            <label class="btn btn-outline-primary w-100 rounded-pill py-2" for="userrole{{ $key }}">
-                                {{ $value }}
-                            </label>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" name="userrole[]" id="userrole{{ $key }}" value="{{ $key }}" {{ in_array($key, old('userrole', [])) ? 'checked' : '' }}>
+                                <label class="form-check-label" for="userrole{{ $key }}">{{ $value }}</label>
+                            </div>
                         </div>
                     @endforeach
                 </div>
