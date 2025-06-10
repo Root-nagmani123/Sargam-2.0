8800fb6825cacd24669d50e222d48c4df5e84799
Mukesh Yadav <mukesh.yadav@awzpact.com>
2025-06-10 12:57:04 +0530

    Excel Download in Faculty Module

--- resources/views/admin/faculty/index.blade.php
+++ resources/views/admin/faculty/index.blade.php
@@ -20,66 +20,14 @@
                         <div class="col-6">
                             <div class="float-end gap-2">
                                 <a href="{{route('faculty.create')}}" class="btn btn-primary">+ Add Faculty</a>
+                                <a href="{{ route('faculty.excel.export') }}" class="btn btn-primary">
+                                    <iconify-icon icon="material-symbols:sim-card-download-rounded" ></iconify-icon> Export Excel
+                                </a>
                             </div>
                         </div>
                     </div>
                     <hr>
-                    <div id="zero_config_wrapper" class="dataTables_wrapper">
-                        <table id="zero_config"
-                            class="table table-striped table-bordered text-nowrap align-middle dataTable"
-                            aria-describedby="zero_config_info">
-                            <thead>
-                                <!-- start row -->
-                                <tr>
-                                    <th>S.No.</th>
-                                    <th>Faculty Type</th>
-                                    <th>Full Name</th>
-                                    <th>Mobile Number</th>
-                                    <th>Current Sector</th>
-                                    <th>Action</th>
-                                    <th>Status</th>
-                                </tr>
-                                <!-- end row -->
-                            </thead>
-                            <tbody>
-                                @if (!empty($faculties) && count($faculties) > 0)
-                                    @foreach ($faculties as $faculty)
-                                        <tr class="odd">
-                                            <td>{{ $loop->iteration }}</td>
-                                            <td>
-                                                @if ($faculty->faculty_type == 1)
-                                                    <span class="badge bg-success">Internal</span>
-                                                @elseif ($faculty->faculty_type == 2)
-                                                    <span class="badge bg-warning">Guest</span>
-                                                @elseif ($faculty->faculty_type == 3)
-                                                    <span class="badge bg-info">Research</span>
-                                                @endif
-                                            <td>{{ $faculty->full_name ?? 'N/A' }}</td>
-                                            <td>{{ $faculty->mobile_no }}</td>
-                                            <td><span class="badge bg-info">{{ $faculty->faculty_sector == 1 ? 'Govt' : 'Private' }}</span></td>
-                                           
-                                            <td>
-                                                <a href="{{ route('faculty.edit', ['id' => encrypt($faculty->pk)]) }}"
-                                                    class="btn btn-primary btn-sm">Edit</a>
-                                                <a href="{{ route('faculty.show', ['id' => encrypt($faculty->pk)]) }}"
-                                                    class="btn btn-info btn-sm">View</a>
-                                            </td>
-                                            <td>
-                                                 <div class="form-check form-switch d-inline-block">
-                                                        <input class="form-check-input status-toggle" type="checkbox" role="switch"
-                                                            data-table="faculty_master" data-column="active_inactive" data-id="{{ $faculty->pk }}" {{ $faculty->active_inactive == 1 ? 'checked' : '' }}>
-                                                    </div>
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                @else
-                                    
-                                @endif
-                               
-                            </tbody>
-                        </table>
-                        
-                    </div>
+                    {!! $dataTable->table(['class' => 'table table-striped table-bordered']) !!}
                 </div>
             </div>
         </div>
@@ -89,3 +37,6 @@
 
 
 @endsection
+@push('scripts')
+    {!! $dataTable->scripts() !!}
+@endpush
