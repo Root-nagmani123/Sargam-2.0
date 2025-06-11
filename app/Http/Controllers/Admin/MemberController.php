fa6738357e45fbefb29935da78cd65b6893ffc67
Mukesh Yadav <mukesh.yadav@awzpact.com>
2025-06-11 16:44:45 +0530

    - member changes

--- app/Http/Controllers/Admin/MemberController.php
+++ app/Http/Controllers/Admin/MemberController.php
@@ -12,6 +12,7 @@ use App\Http\Requests\Admin\Member\{
     StoreMemberStep5Request,
 };
 use Illuminate\Support\Facades\Validator;
+use App\Models\EmployeeMaster;
 
 class MemberController extends Controller
 {
@@ -25,7 +26,35 @@ class MemberController extends Controller
     {
         return view('admin.member.create');
     }
+    private function saveStepData(Request $request, $step)
+    {
+        if($step == 1) {
+            EmployeeMaster::create([
+                'title'         => $request->title,
+                'first_name'    => $request->first_name,
+                'middle_name'   => $request->middle_name,
+                'last_name'     => $request->last_name,
+                'father_name'   => $request->father_husband_name,
+                'marital_status'=> $request->marital_status,
+                'gender'        => $request->gender,
+                'caste_category_pk' => $request->caste_category,
+                'height'        => $request->height,
+                'dob'           => $request->date_of_birth,
+            ]);
+        }
 
+        if($step == 2) {
+            // dd($request->all());
+            [
+                'emp_type'      => $request->type, // Employee Type
+                'emp_id'        => $request->id, // Employee ID
+                'emp_group_pk'  => $request->group, // Employee Group
+                'designation_master_pk' =>$request->designation, // Designation
+                '' =>$request->userid,
+                'department_master_pk' => $request->section // department
+            ];
+        }
+    }
     public function validateStep(Request $request, $step)
     {
         $rules = [];
@@ -33,15 +62,19 @@ class MemberController extends Controller
         switch ($step) {
             case 1:
                 $validator = Validator::make($request->all(), (new StoreMemberStep1Request())->rules());
+                $this->saveStepData($request, 1);
                 break;
             case 2:
                 $validator = Validator::make($request->all(), (new StoreMemberStep2Request())->rules());
+                $this->saveStepData($request, 2);
                 break;
             case 3:
                 $validator = Validator::make($request->all(), (new StoreMemberStep3Request())->rules());
+                $this->saveStepData($request, 3);
                 break;
             case 4:
                 $validator = Validator::make($request->all(), (new StoreMemberStep4Request())->rules());
+                $this->saveStepData($request, 4);
                 break;
             case 5:
                 $validator = Validator::make($request->all(), (new StoreMemberStep5Request())->rules());
@@ -54,7 +87,7 @@ class MemberController extends Controller
         //     return response()->json(['errors' => $validator->errors()], 422);
         // }
 
-        return response()->json(['message' => 'Step validated'], 200);
+        // return response()->json(['message' => 'Step validated'], 200);
     }
 
 
