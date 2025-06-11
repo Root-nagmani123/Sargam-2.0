38664884815287563c9aaf8e8d6bee1b77314d53
Mukesh Yadav <mukesh.yadav@awzpact.com>
2025-06-11 10:28:19 +0530

    Attendance Module Enhancement

--- resources/views/admin/attendance/index.blade.php
+++ resources/views/admin/attendance/index.blade.php
@@ -19,8 +19,7 @@
                     </div>
                     <hr>
                     <div class="row">
-
-                        <div class="col-sm-6">
+                        <div class="col-sm-3">
                             <label for="programme" class="form-label">Course Name :</label>
                             <div class="mb-3">
                                 <select name="course_master_pk" id="programme" class="form-select select2" required>
@@ -32,7 +31,7 @@
                                 <small class="form-text text-muted">Select Course</small>
                             </div>
                         </div>
-                        <div class="col-sm-6">
+                        <div class="col-sm-3">
                             <label for="from_date" class="form-label">From Date :</label>
                             <div class="mb-3">
                                 <input type="date" class="form-control" id="from_date" name="from_date"
@@ -40,24 +39,61 @@
                                 <small class="form-text text-muted">Select From Date</small>
                             </div>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-sm-6">
+
+                        <div class="col-sm-3">
                             <label for="to_date" class="form-label">To Date :</label>
                             <div class="mb-3">
                                 <input type="date" class="form-control" id="to_date" name="to_date" placeholder="To Date">
                                 <small class="form-text text-muted">Select To Date</small>
                             </div>
                         </div>
-                        <div class="col-sm-6">
-                            <label for="view_type" class="form-label">View Type :</label>
+                        <div class="col-sm-3">
+                            <label class="form-label">Attendance Type :</label>
                             <div class="mb-3">
-                                <select name="view_type" id="view_type" class="form-select" required>
-                                    <option value="1">All</option>
-                                    <option value="2">Attendance Taken</option>
-                                    <option value="3">Attendance Not Taken</option>
+                                <div class="form-check form-check-inline" style="vertical-align: middle;">
+                                    <input class="form-check-input" type="radio" name="attendance_type" id="full_day"
+                                        value="full_day" checked>
+                                    <label class="form-check-label" for="full_day">Full Day</label>
+                                </div>
+                                <div class="form-check form-check-inline" style="vertical-align: middle;">
+                                    <input class="form-check-input" type="radio" name="attendance_type" id="manual"
+                                        value="manual">
+                                    <label class="form-check-label" for="manual">Manual</label>
+                                </div>
+                                <div class="form-check form-check-inline" style="vertical-align: middle;">
+                                    <input class="form-check-input" type="radio" name="attendance_type" id="normal"
+                                        value="normal">
+                                    <label class="form-check-label" for="normal">Normal</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-sm-3" id="normal_session_container" style="display: none;">
+                            <label for="session" class="form-label">Normal Session :</label>
+                            <div class="mb-3">
+                                <select name="session" id="session" class="form-select select2">
+                                    <option value="">Select Session</option>
+                                    @foreach($sessions as $session)
+                                        <option value="{{ $session['pk'] }}">{{ $session['shift_name'] }}</option>
+                                    @endforeach
                                 </select>
-                                <small class="form-text text-muted">Select View Type</small>
+                                <small class="form-text text-muted">Select Session</small>
+                            </div>
+                        </div>
+
+                        <div class="col-sm-3" id="manual_session_container" style="display: none;">
+                            <label for="manual_session" class="form-label">Manual Session:</label>
+                            <div class="mb-3">
+                                <select name="manual_session" id="manual_session" class="form-select select2">
+                                    <option value="">Select Session</option>
+                                    @foreach($maunalSessions as $maunalSession)
+                                        <option value="{{ $maunalSession['class_session'] }}">
+                                            {{ $maunalSession['class_session'] }}</option>
+                                    @endforeach
+                                </select>
+                                <small class="form-text text-muted">Select Session</small>
                             </div>
                         </div>
                     </div>
