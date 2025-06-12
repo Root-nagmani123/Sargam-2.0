0f5470babfcc87d43215df959bbcad9c131394d2
nibhashlbsnaa <nibhash.mishra@awzpact.com>
2025-06-12 11:09:38 +0530

    fc new design

---
+++ resources/views/fc/front_page.blade.php
@@ -0,0 +1,160 @@
+@extends('fc.layouts.master')
+
+@section('title', 'Foundation Course | Lal Bahadur Shastri National Academy of Administration')
+
+@section('content')
+
+<!-- Main Content Box -->
+<div class="academy-box">
+    <div class="text-center mb-2">
+        <img src="{{asset('images/lbsnaa_logo.jpg')}}" alt="LBSNAA Logo" height="80">
+        <h4 class="mt-3 fw-bold" style="color: #af2910;font-size: 20px;">Lal Bahadur Shastri National Academy of
+            Administration</h4>
+        <p class="text-muted" style="font-size: 16px;">Mussoorie, Uttarakhand</p>
+    </div>
+    <hr>
+    <div class="container">
+        <div class="text-center mb-4">
+            <h5 class="text-primary fw-bold mt-4 mb-2"><a href="#" class="text-decoration-none"
+                    style="color: #004a93;font-size: 20px;">Congratulations</a></h5>
+            <h4 class="fw-semibold mt-2" style="font-size: 20px;">100th Foundation Course for IAS/IPS/IFoS Officers
+            </h4>
+        </div>
+        <div class="row g-3">
+            <!-- Course Duration -->
+            <div class="col-md-6">
+                <div class="d-flex align-items-center border rounded-4 p-3 h-100">
+                    <i class="material-icons menu-icon me-3 fs-3" style="color: #004a93;">calendar_today</i>
+                    <div>
+                        <h6 class="mb-1 fw-semibold">Course Duration</h6>
+                        <div class="text-muted">August 25th, 2025 – November 28th, 2025</div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Online Registration -->
+            <div class="col-md-6">
+                <div class="d-flex align-items-center border rounded-4 p-3 h-100">
+                    <i class="material-icons menu-icon me-3 fs-3" style="color: #004a93;">location_on</i>
+                    <div>
+                        <h6 class="mb-1 fw-semibold">Online Registration</h6>
+                        <div class="text-muted">July 18th, 2025 – July 28th, 2025</div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Online Exemption -->
+            <div class="col-md-6">
+                <div class="d-flex align-items-center border rounded-4 p-3 h-100">
+                    <i class="material-icons menu-icon me-3 fs-3" style="color: #004a93;">group</i>
+
+                    <div>
+                        <h6 class="mb-1 fw-semibold">Online Exemption</h6>
+                        <div class="text-muted">Available after registration</div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Laptop Requirement -->
+            <div class="col-md-6">
+                <div class="d-flex align-items-center border rounded-4 p-3 h-100">
+                    <i class="material-icons menu-icon me-3 fs-3" style="color: #004a93;">laptop_windows</i>
+                    <div>
+                        <h6 class="mb-1 fw-semibold">Laptop Requirement</h6>
+                        <div class="text-muted">Mandatory for all participants</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="notice-box mt-4">
+        <p class="fw-bold" style="color: #004a93;">Important Notice:</p>
+        <ul style="color: #004a93;">
+            <li>As you stand on the threshold of a new chapter in Public services in your lives, I extend my heartfelt
+                congratulations on your hard-earned success in one of the toughest examinations. Your selection for this
+                righteous path signifies not just an achievement, but a calling to embody the spirit of selfless service
+                and unwavering dedication. The Academy congratulates you on this journey of lifelong learning and
+                service to the nation with a lot of hope and warmth.</li>
+            <li>Your journey as Officer Trainees into the realm of governance begins with the Foundation Course,
+                orchestrated by the Lal Bahadur Shastri National Academy of Administration (LBSNAA) in Mussoorie. This
+                cornerstone training program, designed to orient new recruits from various civil services, lays the
+                foundation upon which the career in civil service is built. However, your participation in the course is
+                subject to the service allocation.</li>
+            <li>The 99th Foundation Course (FC) will begin on August 26th, 2024 (Monday) and culminate on November 29th,
+                2024 (Friday). You will be required to report at the Academy a couple of days prior for the completion
+                of joining formalities. Given the likely large number of course participants, the aforementioned
+                processes shall be conducted in a staggered manner over two days (between August 24th and August 25th,
+                2024) for smaller batches of Officer Trainees. Details of reporting dates and names of those expected to
+                report on the given dates will be posted on the Academy's website in due course.</li>
+            <li>A prerequisite to participate in the 99th Foundation Course is to register for it on the Academy's
+                portal. Please peruse the enclosed instructions to register yourself for the 99th FC. Registration will
+                open on July 18th and close on July 28th, 2024. Further, please note that at this point, it would
+                suffice to fill in the Descriptive Roll component of the registration form. (Other forms may be filled
+                and documents uploaded post the allocation of service by the Department of Personnel and Training
+                (DoPT), Gol. You are advised to visit https://www.lbsnaa.gov.in in regularly for further updates. You
+                may also write to us at support.lbsnaa@nic.in for registration related queries).</li>
+            <li>
+                If you wish to seek exemption from the course, you will be required to apply on the same portal (please
+                read registration instructions for details). Further, we are aware that some of you may want to wait for
+                the allocation of service by DoPT to decide whether to register for the course. Accordingly the last
+                date for registration i.e. July 28th, 2024 shall be extended if the service allocation list is not
+                released by DoPT till that date. Others may please proceed with registration forthwith and upload
+                documents as required.
+            </li>
+            <li>We are also sharing herewith, documents that will help you understand requirements and expectations from
+                Officer Trainees on different aspects of the Foundation course (The documents will be visible after
+                registration for 99th Foundation Course). Please peruse these documents carefully and fill the
+                registration form -</li>
+            <ul class="list-unstyled ps-3">
+                <li class="border-bottom fw-bold">Annexure I - About the 99th Foundation Course</li>
+                <li class="border-bottom fw-bold">Annexure II - Joining formalities and information</li>
+                <li class="border-bottom fw-bold">Annexure III - Clothing, dress code and miscellaneous matters.</li>
+                <li class="border-bottom fw-bold">Annexure IV - Subscription of Clubs Societies and Houses.</li>
+                <li class="border-bottom fw-bold">Annexure V - Check list for submission of forms/ documents</li>
+            </ul>
+            <li>The Academy is committed to securing a barrier-free and enabling environment for differently abled
+                candidates. Course participants may please contact - support.lbsnaa@nic.in for further communication on
+                the matter.</li>
+            <li>It may also be noted that in addition to registration, a mandatory "Prarambh MOOC module based on
+                selected courses from IGOT is to be completed before 15th August, 2024 and the certificates to be
+                uploaded in Gyan Portal. Please note that this will be part of the final evaluation. Details related to
+                module MOOCs have to be accessed on Gyan Portal.</li>
+            <li>Further, please note that every course participant must carry a personal laptop preloaded with the
+                following applications/ softwares:
+                <ul class="list-unstyled ps-3">
+                    <li class="border-bottom fw-bold">Windows 10 /11.</li>
+                    <li class="border-bottom fw-bold">MS office</li>
+                    <li class="border-bottom fw-bold">Project Management Software</li>
+                    <li class="border-bottom fw-bold">MS Office 2016 or higher version. MS office should not be starter version. It must include MS
+                        Word, MS Power Point and MS Excel.
+                        MS Project 2016.</li>
+                </ul>
+            </li>
+            <li>The Academy will not be able to provide PCs/laptops for the ICT classes and it is mandatory for course
+                participants to bring their own laptops for ICT classes.</li>
+            <li>Finally, it is important to underscore at the outset that the 99th Foundation Course will be fairly
+                rigorous with substantial academic input, co- curricular and extra curricular activities, field
+                immersions and a plethora of cultural/social/outdoor events densely packed into a tight schedule. Please
+                do not expect the luxury of leisure or time. Also, please attend to pressing commitments or engagements
+                if any, before the commencement of the course, for the 99th FC will be a NO LEAVE course.</li>
+        </ul>
+    </div>
+    <p class="my-3">The 99th FC at the academy lays special emphasis on conduct and shishtachar of the
+        Officer Trainees. You are advised to go through the shishtachar manual, undertakings and course manual to
+        apprise yourself and prepare yourself according to the desired expectations.</p>
+        <p class="text-center my-3">On behalf of the Academy and the Course Team, I wish you the best and look forward to meeting you at the Academy soon.</p>
+
+        <div class="signature mt-5">
+        <p class="text-muted">Shelesh Nawal<br>
+            Deputy Director (Sr.)<br>
+            Course Coordinator, 99th FC
+        </p>
+    </div>
+    <div class="text-center mt-4">
+        <a href="#" class="btn btn-primary px-4" style="background-color: #004a93; border: #004a93;">Click Here to
+            Proceed</a>
+    </div>
+    
+</div>
+@endsection
---
+++ resources/views/fc/layouts/header.blade.php
@@ -0,0 +1,82 @@
+   <!-- Top Blue Bar (Govt of India) -->
+    <div class="top-header d-flex justify-content-between align-items-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 d-flex align-items-center">
+                    <img src="https://upload.wikimedia.org/wikipedia/en/thumb/4/41/Flag_of_India.svg/800px-Flag_of_India.svg.png" alt="GoI Logo" height="30">
+                    <span class="ms-2" style="font-family: roboto; font-size: 14px;">Government of India</span>
+                </div>
+                <div class="col-md-9 text-end d-flex justify-content-end align-items-center">
+                    <ul class="nav justify-content-end align-items-center">
+                        <li class="nav-item"><a href="#" class="text-white text-decoration-none"
+                                style=" font-size: 12px;">Skip to Main Content</a></li>
+                        <span class="text-muted me-3 ms-3">|</span>
+                        <li class="nav-item"><a href="#" class="text-white text-decoration-none"><span class="ms-1"
+                                    style=" font-size: 12px;">Screen Reader</span></a></li>
+                        <span class="text-muted me-3 ms-3">|</span>
+                        <li class="nav-item"><a href="#" class="text-white text-decoration-none me-3 ms-3"
+                                style=" font-size: 12px;">A+</a></li>
+                        <li class="nav-item"><a href="#" class="text-white text-decoration-none me-3 ms-3"
+                                style=" font-size: 12px;">A</a></li>
+                        <li class="nav-item"><a href="#" class="text-white text-decoration-none me-3 ms-3"
+                                style=" font-size: 12px;">A-</a></li>
+                        <span class="text-muted me-3 ms-3">|</span>
+                        <li class="nav-item"><a href="#" class="text-white text-decoration-none"><i class="material-icons fs-5">contrast</i></a></li>
+                        <span class="text-muted me-3 ms-3">|</span>
+                        <li class="nav-item"><a href="#" class="text-white text-decoration-none"><i class="material-icons fs-5">contrast</i>
+                                <span><select name="lang" id="" class="form-select form-select-sm"
+                                        style="width: 100px; display: inline-block; font-size: 14px;  background-color: transparent; border: none;color: #fff;font-family: 'roboto', Courier, monospace;font-size: 12px;">"
+                                        <option value="">Language</option>
+                                        <option value="en" selected>English</option>
+                                    </select></span></a></li>
+                        <span class="text-muted me-3 ">|</span>
+                        <li>
+                            <a id="uw-widget-custom-trigger" contenteditable="false" style="cursor: pointer;">
+                                 <span class="text-white ms-1"
+                                    style=" font-size: 12px;">
+                                    More
+                                </span>
+                            </a>
+
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Sticky Header -->
+    <div class="header sticky-top bg-white shadow-sm">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg">
+                <div class="container-fluid px-0">
+                    <a class="navbar-brand me-2" href="#">
+                        <img src="https://i.pinimg.com/736x/a8/fa/ef/a8faef978e6230b6a12d1c29c62d5edf.jpg" alt="Logo 1" height="40">
+                    </a>
+                    <span class="vr mx-2"></span>
+                    <a class="navbar-brand" href="#">
+                        <img src="https://www.lbsnaa.gov.in/admin_assets/images/logo.png" alt="Logo 2" height="40">
+                    </a>
+
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                        <ul class="navbar-nav align-items-center">
+                            <li class="nav-item">
+                                <a class="nav-link ms-4 me-4" href="#">About Us</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link ms-4 me-4" href="#">FAQs</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="btn btn-outline-primary ms-4" href="#">Login</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </div>
