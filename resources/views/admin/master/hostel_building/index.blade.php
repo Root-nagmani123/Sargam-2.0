8ae81f0b8e85ed7dc3caa744acc973772489d340
Mukesh Yadav <mukesh.yadav@awzpact.com>
2025-06-23 12:29:47 +0530

    Hostel Building Master Module

---
+++ resources/views/admin/master/hostel_building/index.blade.php
@@ -0,0 +1,39 @@
+@extends('admin.layouts.master')
+
+@section('title', 'Hostel Building')
+
+@section('content')
+<div class="container-fluid">
+
+    <x-breadcrum title="Hostel Building" />
+    <x-session_message />
+
+    <div class="datatables">
+        <!-- start Zero Configuration -->
+        <div class="card">
+            <div class="card-body">
+                <div class="table-responsive">
+                    <div class="row">
+                        <div class="col-6">
+                            <h4>Hostel Building</h4>
+                        </div>
+                        <div class="col-6">
+                            <div class="float-end gap-2">
+                                <a href="{{route('master.hostel.building.create')}}" class="btn btn-primary">+ Add Hostel Building</a>
+                            </div>
+                        </div>
+                    </div>
+                    <hr>
+                    {{ $dataTable->table(['class' => 'table table-striped table-bordered']) }}
+                </div>
+            </div>
+        </div>
+        <!-- end Zero Configuration -->
+    </div>
+</div>
+
+
+@endsection
+@push('scripts')
+{{ $dataTable->scripts() }}
+@endpush
