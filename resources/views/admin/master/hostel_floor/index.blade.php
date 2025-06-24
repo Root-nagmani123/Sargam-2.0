46a319b0d3fe5d3ed499257bb0b19c918d5f3788
Mukesh Yadav <mukesh.yadav@awzpact.com>
2025-06-24 11:42:12 +0530

    Hostel Floor and Hostel Room Master

---
+++ resources/views/admin/master/hostel_floor/index.blade.php
@@ -0,0 +1,39 @@
+@extends('admin.layouts.master')
+
+@section('title', 'Hostel Floor')
+
+@section('content')
+<div class="container-fluid">
+
+    <x-breadcrum title="Hostel Floor" />
+    <x-session_message />
+
+    <div class="datatables">
+        <!-- start Zero Configuration -->
+        <div class="card">
+            <div class="card-body">
+                <div class="table-responsive">
+                    <div class="row">
+                        <div class="col-6">
+                            <h4>Hostel Floor</h4>
+                        </div>
+                        <div class="col-6">
+                            <div class="float-end gap-2">
+                                <a href="{{route('master.hostel.floor.create')}}" class="btn btn-primary">+ Add Hostel Floor</a>
+                            </div>
+                        </div>
+                    </div>
+                    <hr>
+                    {{ $dataTable->table(['class' => 'table table-striped table-bordered']) }}
+                </div>
+            </div>
+        </div>
+        <!-- end Zero Configuration -->
+    </div>
+</div>
+
+
+@endsection
+@push('scripts')
+{{ $dataTable->scripts() }}
+@endpush
---
+++ resources/views/admin/master/hostel_room/create.blade.php
@@ -0,0 +1,69 @@
+@extends('admin.layouts.master')
+
+@section('title', 'Hostel Room')
+
+@section('content')
+
+<div class="container-fluid">
+    <x-breadcrum title="Hostel Room" />
+    <x-session_message />
+    <!-- start Vertical Steps Example -->
+    <div class="card">
+        <div class="card-body">
+            <h4 class="card-title mb-3">
+                {{ !empty($hostelRoomMaster) ? 'Edit Hostel Room' : 'Create Hostel Room' }}
+            </h4>
+            <hr>
+            <form action="{{ route('master.hostel.room.store') }}" method="POST" id="hostelRoomForm">
+                @csrf
+                @if(!empty($hostelRoomMaster)) 
+                    <input type="hidden" name="pk" value="{{ encrypt($hostelRoomMaster->pk) }}">
+                @endif
+                <div class="row">
+                    
+                    <div class="col-md-4">
+                        <div class="mb-3">
+                            <x-input
+                                name="hostel_room_name"
+                                label="Name :" 
+                                placeholder="Name" 
+                                formLabelClass="form-label"
+                                required="true"
+                                value="{{ old('hostel_room_name', $hostelRoomMaster->hostel_room_name ?? '') }}"
+                                />
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="mb-3">
+                            <x-input
+                                name="capacity"
+                                label="Capacity :" 
+                                placeholder="Capacity" 
+                                formLabelClass="form-label"
+                                required="true"
+                                value="{{ old('capacity', $hostelRoomMaster->capacity ?? '') }}"
+                                />
+                        </div>
+                    </div>
+                    
+                </div>
+                <hr>
+                <div class="mb-3">
+                    <button class="btn btn-primary hstack gap-6 float-end" type="submit" id="saveClassSessionForm">
+                        <i class="material-icons menu-icon">save</i>
+                        {{ !empty($hostelRoomMaster) ? 'Update' : 'Save' }}
+                    </button>
+                    <a href="{{ route('master.hostel.room.index') }}" class="btn btn-secondary hstack gap-6 float-end me-2">
+                        <i class="material-icons menu-icon">arrow_back</i>
+                        Back
+                    </a>
+                </div>
+            </form>
+        </div>
+    </div>
+    <!-- end Vertical Steps Example -->
+</div>
+
+
+@endsection
