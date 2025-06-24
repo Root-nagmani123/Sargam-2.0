af6609e80225d94ebbb343f1c6a1c78e955e34c6
hardeeplbsnaa <hardeep.dagar@awzpact.com>
2025-06-24 14:53:11 +0530

    bug fix

--- app/Http/Controllers/Admin/Registration/FrontPageController.php
+++ app/Http/Controllers/Admin/Registration/FrontPageController.php
@@ -353,7 +353,6 @@ class FrontPageController extends Controller
 
     public function exemptionIndex()
     {
-       dd( auth()->check() ? auth()->id() : null);
 
         $headings = ExemptionCategory::with(['creator', 'updater'])
             ->where('is_notice', 0)
