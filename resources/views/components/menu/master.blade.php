d72077deb445642dff18556e51523384325e49ad
Mukesh Yadav <mukesh.yadav@awzpact.com>
2025-06-24 14:33:44 +0530

    Menu

---
+++ resources/views/components/menu/master.blade.php
@@ -0,0 +1,169 @@
+<nav class="sidebar-nav scroll-sidebar" id="menu-right-mini-3" data-simplebar="">
+    <ul class="sidebar-menu" id="sidebarnav">
+        <!-- ---------------------------------- -->
+        <!-- Home -->
+        <!-- ---------------------------------- -->
+        <li class="nav-small-cap">
+            <span class="hide-menu">Master</span>
+        </li>
+        <li class="sidebar-item">
+            <a class="sidebar-link" href="{{ route('master.country.index') }}">
+                <iconify-icon icon="solar:airbuds-case-line-duotone">
+                </iconify-icon>
+                <span class="hide-menu">Country</span>
+            </a>
+        </li>
+        <li class="sidebar-item">
+            <a class="sidebar-link" href="{{ route('master.state.index') }}">
+                <iconify-icon icon="material-symbols:distance">
+                </iconify-icon>
+                <span class="hide-menu">State</span>
+            </a>
+        </li>
+        <li class="sidebar-item">
+            <a class="sidebar-link" href="{{ route('master.district.index') }}">
+                <iconify-icon icon="arcticons:district">
+                </iconify-icon>
+                <span class="hide-menu">District</span>
+            </a>
+        </li>
+        <li class="sidebar-item">
+            <a class="sidebar-link" href="{{ route('master.city.index') }}" id="get-url" aria-expanded="false">
+                <iconify-icon icon="solar:city-bold-duotone">
+                </iconify-icon>
+                <span class="hide-menu">City</span>
+            </a>
+        </li>
+        <li class="sidebar-item">
+            <a class="sidebar-link" href="{{ route('master.faculty.expertise.index') }}" id="get-url"
+                aria-expanded="false">
+                <iconify-icon icon="solar:face-scan-square-broken"></iconify-icon>
+                <span class="hide-menu">Faculty Expertise</span>
+            </a>
+        </li>
+        <li class="sidebar-item">
+            <a class="sidebar-link" href="{{ route('Venue-Master.index') }}" id="get-url" aria-expanded="false">
+                <iconify-icon icon="solar:face-scan-square-broken"></iconify-icon>
+                <span class="hide-menu">Venue Master</span>
+            </a>
+        </li>
+        <li class="sidebar-item">
+            <a class="sidebar-link" href="{{ route('master.class.session.index') }}" id="get-url" aria-expanded="false">
+                <iconify-icon icon="solar:face-scan-square-broken"></iconify-icon>
+                <span class="hide-menu">Class Session</span>
+            </a>
+        </li>
+        <li class="sidebar-item">
+            <a class="sidebar-link" href="{{ route('subject-module.index') }}" id="get-url" aria-expanded="false">
+                <iconify-icon icon="solar:widget-4-line-duotone">
+                </iconify-icon>
+                <span class="hide-menu">Subject Module</span>
+            </a>
+        </li>
+        <li class="sidebar-item">
+            <a class="sidebar-link" href="{{ route('group.mapping.index') }}" id="get-url" aria-expanded="false">
+                <iconify-icon icon="solar:calendar-mark-line-duotone">
+                </iconify-icon>
+                <span class="hide-menu">Group Mapping</span>
+            </a>
+        </li>
+        <li class="sidebar-item">
+            <a class="sidebar-link" href="{{ route('master.course.group.type.index') }}" id="get-url"
+                aria-expanded="false">
+                <iconify-icon icon="solar:face-scan-square-broken"></iconify-icon>
+                <span class="hide-menu">Course Group Type</span>
+            </a>
+        </li>
+        <li class="sidebar-item">
+            <a class="sidebar-link" href="{{ route('programme.index') }}" id="get-url" aria-expanded="false">
+                <iconify-icon icon="solar:mask-happly-line-duotone">
+                </iconify-icon>
+                <span class="hide-menu">Course Master</span>
+            </a>
+        </li>
+        <li class="sidebar-item">
+            <a class="sidebar-link" href="{{ route('master.mdo_duty_type.index') }}" id="get-url" aria-expanded="false">
+                <iconify-icon icon="solar:face-scan-square-broken"></iconify-icon>
+                <span class="hide-menu">MDO Duty Type</span>
+            </a>
+        </li>
+        <li class="sidebar-item">
+            <a class="sidebar-link" href="{{ route('master.faculty.type.master.index') }}" id="get-url"
+                aria-expanded="false">
+                <iconify-icon icon="solar:face-scan-square-broken"></iconify-icon>
+                <span class="hide-menu">Faculty Type</span>
+            </a>
+        </li>
+        <li class="sidebar-item">
+            <a class="sidebar-link" href="{{ route('master.exemption.category.master.index') }}" id="get-url"
+                aria-expanded="false">
+                <iconify-icon icon="solar:face-scan-square-broken"></iconify-icon>
+                <span class="hide-menu">Exemption Category</span>
+            </a>
+        </li>
+        <li class="sidebar-item">
+            <a class="sidebar-link" href="{{ route('master.exemption.medical.speciality.index') }}" id="get-url"
+                aria-expanded="false">
+                <iconify-icon icon="solar:face-scan-square-broken"></iconify-icon>
+                <span class="hide-menu">Exemption Medical Speciality</span>
+            </a>
+        </li>
+        <li class="sidebar-item">
+            <a class="sidebar-link" href="{{ route('admin.fc_exemption.index') }}">
+                <iconify-icon icon="solar:airbuds-case-line-duotone">
+                </iconify-icon>
+                <span class="hide-menu">Fc Exemption Master</span>
+            </a>
+        </li>
+
+        <li class="sidebar-item">
+            <a class="sidebar-link" href="{{ route('master.memo.type.master.index') }}">
+                <iconify-icon icon="solar:airbuds-case-line-duotone">
+                </iconify-icon>
+                <span class="hide-menu">Memo Type Master</span>
+            </a>
+        </li>
+        <li class="sidebar-item">
+            <a class="sidebar-link" href="{{ route('master.memo.conclusion.master.index') }}">
+                <iconify-icon icon="solar:airbuds-case-line-duotone">
+                </iconify-icon>
+                <span class="hide-menu">Memo Conclusion Master</span>
+            </a>
+        </li>
+        <li class="sidebar-item">
+            <a class="sidebar-link" href="{{ route('master.department.master.index') }}">
+                <iconify-icon icon="solar:airbuds-case-line-duotone">
+                </iconify-icon>
+                <span class="hide-menu">Department Master</span>
+            </a>
+        </li>
+        <li class="sidebar-item">
+            <a class="sidebar-link" href="{{ route('master.designation.index') }}">
+                <iconify-icon icon="solar:airbuds-case-line-duotone">
+                </iconify-icon>
+                <span class="hide-menu">Designation Master</span>
+            </a>
+        </li>
+        <li class="sidebar-item">
+            <a class="sidebar-link" href="{{ route('master.employee.type.index') }}">
+                <iconify-icon icon="solar:airbuds-case-line-duotone">
+                </iconify-icon>
+                <span class="hide-menu">Employee Type</span>
+            </a>
+        </li>
+        <li class="sidebar-item">
+            <a class="sidebar-link" href="{{ route('master.employee.group.index') }}">
+                <iconify-icon icon="solar:airbuds-case-line-duotone">
+                </iconify-icon>
+                <span class="hide-menu">Employee Group</span>
+            </a>
+        </li>
+        <li class="sidebar-item">
+            <a class="sidebar-link" href="{{ route('master.caste.category.index') }}">
+                <iconify-icon icon="solar:airbuds-case-line-duotone">
+                </iconify-icon>
+                <span class="hide-menu">Caste Category</span>
+            </a>
+        </li>
+    </ul>
+</nav>
