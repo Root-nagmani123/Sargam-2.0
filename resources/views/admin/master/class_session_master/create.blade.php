dd732ffbf8e439c4d6796466b19af1de9c8e3164
nibhashlbsnaa <nibhash.mishra@awzpact.com>
2025-07-02 12:52:52 +0530

    ui chnages

--- resources/views/admin/master/class_session_master/create.blade.php
+++ resources/views/admin/master/class_session_master/create.blade.php
@@ -8,7 +8,7 @@
     <x-breadcrum title="Class Session Master" />
     <x-session_message />
     <!-- start Vertical Steps Example -->
-    <div class="card">
+    <div class="card" style="border-left: 4px solid #004a93;">
         <div class="card-body">
             <h4 class="card-title mb-3">
                 {{ !empty($classSessionMaster) ? 'Edit Class Session' : 'Create Class Session' }}
@@ -61,13 +61,11 @@
                     </div>
                 </div>
                 <hr>
-                <div class="mb-3">
-                    <button class="btn btn-primary hstack gap-6 float-end" type="submit" id="saveClassSessionForm">
-                        <i class="material-icons menu-icon">save</i>
+                <div class="mb-3 text-end gap-3">
+                    <button class="btn btn-primary" type="submit" id="saveClassSessionForm">
                         {{ !empty($classSessionMaster) ? 'Update' : 'Save' }}
                     </button>
-                    <a href="{{ route('master.class.session.index') }}" class="btn btn-secondary hstack gap-6 float-end me-2">
-                        <i class="material-icons menu-icon">arrow_back</i>
+                    <a href="{{ route('master.class.session.index') }}" class="btn btn-secondary">
                         Back
                     </a>
                 </div>
--- resources/views/admin/subject_module/create.blade.php
+++ resources/views/admin/subject_module/create.blade.php
@@ -5,31 +5,10 @@
 @section('content')
 
 <div class="container-fluid">
-    <div class="card card-body py-3">
-        <div class="row align-items-center">
-            <div class="col-12">
-                <div class="d-sm-flex align-items-center justify-space-between">
-                    <h4 class="mb-4 mb-sm-0 card-title">Add Subject module</h4>
-                    <nav aria-label="breadcrumb" class="ms-auto">
-                        <ol class="breadcrumb">
-                            <li class="breadcrumb-item d-flex align-items-center">
-                                <a class="text-muted text-decoration-none d-flex" href="index.html">
-                                    <iconify-icon icon="solar:home-2-line-duotone" class="fs-6"></iconify-icon>
-                                </a>
-                            </li>
-                            <li class="breadcrumb-item" aria-current="page">
-                                <span class="badge fw-medium fs-2 bg-primary-subtle text-primary">
-                                    Subject module
-                                </span>
-                            </li>
-                        </ol>
-                    </nav>
-                </div>
-            </div>
-        </div>
-    </div>
+    <x-breadcrum title="Subject module" />
+    <x-session_message />
     <!-- start Vertical Steps Example -->
-    <div class="card">
+    <div class="card" style="border-left: 4px solid #004a93;">
         <div class="card-body">
             <h4 class="card-title mb-3">Subject module</h4>
             <hr>
