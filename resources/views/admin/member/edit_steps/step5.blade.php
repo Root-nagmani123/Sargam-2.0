797cb2464db29390f52dffea97e18f4c1d45b237
Mukesh Yadav <mukesh.yadav@awzpact.com>
2025-07-18 11:36:40 +0530

    Bug Fixing

--- resources/views/admin/member/edit_steps/step5.blade.php
+++ resources/views/admin/member/edit_steps/step5.blade.php
@@ -23,6 +23,9 @@
             <label class="form-label" for="picture">Upload Picture :</label>
             <input type="file" class="form-control" id="picture" name="picture">
         </div>
+        <small class="text-muted d-block">
+            Allowed file types: <strong>JPG</strong>, <strong>PNG</strong>, <strong>JPEG</strong> | Max file size: <strong>500 KB</strong>
+        </small>
         {!! $member->profile_picture ? '<a href="' . asset('storage/' . $member->profile_picture) . '" target="_blank" class="btn btn-primary">View Picture</a>' : '' !!}
     </div>
     <div class="col-md-6">
@@ -30,6 +33,9 @@
             <label class="form-label" for="additionaldocument">Additional Document Upload :</label>
             <input type="file" class="form-control" id="additionaldocument" name="additionaldocument">
         </div>
+        <small class="text-muted d-block">
+            Allowed file types: <strong>PDF</strong>, <strong>JPG</strong>, <strong>PNG</strong> | Max file size: <strong>1 MB</strong>
+        </small>
         {!! $member->additional_doc_upload ? '<a href="' . asset('storage/' . $member->additional_doc_upload) . '" target="_blank" class="btn btn-primary">View Document</a>' : '' !!}
     </div>
 </div>
