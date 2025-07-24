2ac0f33bc75923ba159b85efb264beb59f9c82e6
Mukesh Yadav <mukesh.yadav@awzpact.com>
2025-07-24 14:50:58 +0530

    - commented code removed

--- app/Http/Controllers/Admin/MemberController.php
+++ app/Http/Controllers/Admin/MemberController.php
@@ -34,23 +34,6 @@ class MemberController extends Controller
         }
     }
 
-    // private function saveStep1Data(Request $request)
-    // {
-        
-    //     return EmployeeMaster::create([
-    //         'title' => $request->title,
-    //         'first_name' => $request->first_name,
-    //         'middle_name' => $request->middle_name,
-    //         'last_name' => $request->last_name,
-    //         'father_name' => $request->father_husband_name,
-    //         'marital_status' => $request->marital_status,
-    //         'gender' => $request->gender,
-    //         'caste_category_pk' => $request->caste_category,
-    //         'height' => $request->height,
-    //         'dob' => $request->date_of_birth,
-    //     ]);
-    // }
-
     private function saveOrUpdateStep1Data(Request $request)
     {
         $data = [
@@ -423,29 +406,6 @@ class MemberController extends Controller
 
     public function updateValidateStep(Request $request, $step, $id)
     {
-        // $request->merge(['emp_id' => ($id)]);
-        // $validatorClass = "App\\Http\\Requests\\Admin\\Member\\StoreMemberStep{$step}Request";
-        // if (!class_exists($validatorClass)) {
-        //     return response()->json(['error' => 'Invalid step'], 400);
-        // }
-
-        // $validator = Validator::make($request->all(), (new $validatorClass())->rules());
-
-        // if ($validator->fails()) {
-        //     return response()->json(['errors' => $validator->errors()], 422);
-        // }
-
-        // $method = "saveOrUpdateStep{$step}Data";
-        // $responseData = method_exists($this, $method) ? $this->{$method}($request) : null;
-
-        // if (!$responseData) {
-        //     return response()->json(['error' => 'Failed to save data'], 500);
-        // }
-
-        // return response()->json([
-        //     'message' => "Step $step validated and data saved.",
-        //     'pk' => $responseData->pk
-        // ], 200);
 
         $request->merge(['emp_id' => $id]);
 
@@ -501,68 +461,6 @@ class MemberController extends Controller
             'pk' => $responseData->pk
         ], 200);
     }
-
-    // function updatedStep1Data(Request $request)
-    // {
-    //     EmployeeMaster::where('pk', (int) $request->emp_id)->update([
-    //         'title' => $request->title,
-    //         'first_name' => $request->first_name,
-    //         'middle_name' => $request->middle_name,
-    //         'last_name' => $request->last_name,
-    //         'father_name' => $request->father_husband_name,
-    //         'marital_status' => $request->marital_status,
-    //         'gender' => $request->gender,
-    //         'caste_category_pk' => $request->caste_category,
-    //         'height' => $request->height,
-    //         'dob' => $request->date_of_birth
-    //     ]);
-    //     return EmployeeMaster::where('pk', (int) $request->emp_id)->first();
-    // }
-
-    // function updatedStep2Data(Request $request)
-    // {
-    //     EmployeeMaster::where('pk', (int) $request->emp_id)->update([  
-    //         'emp_type' => $request->type, // Employee Type
-    //         'emp_id' => $request->id, // Employee ID
-    //         'emp_group_pk' => $request->group, // Employee Group
-    //         'designation_master_pk' => $request->designation, // Designation
-    //         'department_master_pk' => $request->section // department
-    //     ]);
-    //     return EmployeeMaster::where('pk', (int) $request->emp_id)->first();
-    // }
-
-    // function updatedStep3Data(Request $request)
-    // {
-    //     return EmployeeMaster::find($request->emp_id);
-    // }
-
-    // function updatedStep4Data(Request $request)
-    // {
-    //     $address = [
-    //         'current_address' => $request->address,
-    //         'country_master_pk' => $request->country,
-    //         'state_master_pk' => $request->state,
-    //         'state_district_mapping_pk' => $request->district,
-    //         'city' => $request->city,
-    //         'zipcode' => $request->postal,
-
-    //         'permanent_address' => $request->permanentaddress,
-    //         'pcountry_master_pk' => $request->permanentcountry,
-    //         'pstate_master_pk' => $request->permanentstate,
-    //         'pstate_district_mapping_pk' => $request->permanentdistrict,
-    //         'pcity' => $request->permanentcity,
-    //         'pzipcode' => $request->permanentpostal,
-
-    //         'email' => $request->personalemail,
-    //         'officalemail' => $request->officialemail,
-    //         'mobile' => $request->mnumber,
-    //         'emergency_contact_no' => $request->emergencynumber,
-    //         'landline_contact_no' => $request->landlinenumber,
-    //     ];
-    //     EmployeeMaster::find($request->emp_id)->update($address);
-    //     return EmployeeMaster::find($request->emp_id);
-    // }
-
     public function excelExport(Request $request)
     {
         $fileName = 'members-'.date('d-m-Y').'.xlsx';
