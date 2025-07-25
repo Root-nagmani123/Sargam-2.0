ae8a68cfd52f03c093ceee6749adace944bb21e0
Mukesh Yadav <mukesh.yadav@awzpact.com>
2025-07-25 15:42:31 +0530

    - fixed

--- resources/views/components/menu/master.blade.php
+++ resources/views/components/menu/master.blade.php
@@ -1,3 +1,5 @@
+
+
 <nav class="sidebar-nav scroll-sidebar" id="menu-right-mini-2" data-simplebar>
     <ul class="sidebar-menu" id="sidebarnav">
         <x-menu.generate-code config="menus.master" />
