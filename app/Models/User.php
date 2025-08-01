5b8a6c93fa9b308a2f01498f0a2caa91c63025fb
hardeeplbsnaa <hardeep.dagar@awzpact.com>
2025-08-01 17:04:02 +0530

    exemption master list fix using pk instead of id form usercredentilas

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,6 +16,7 @@ class User extends Authenticatable
     use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
     protected $table = 'user_credentials';
+    protected $primaryKey = 'pk';
 
 
     /**
