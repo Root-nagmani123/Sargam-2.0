fc9a7c26c8bb85766d35055c26c70d05fc265a34
hardeeplbsnaa <hardeep.dagar@awzpact.com>
2025-09-04 17:09:32 +0530

    master integration

--- resources/views/admin/forms/field-types.blade.php
+++ resources/views/admin/forms/field-types.blade.php
@@ -1,72 +1,27 @@
-<!-- resources/views/forms/field-types.blade.php -->
-{{-- @php
-    $validFieldHeadings = [
-        'country','state','district','language','admissioncategory','stream','institution','jobtype','boardname','qualification',
-        'religion_master_pk','last_service_pk','sports','size','fcscale','distinction','fatherprofession',
-        'shoessize','studentskill','birth_district','birth_state','birth_country','pdistrict_id','mdistrict_id','admission_category_pk',
-        'highest_stream_pk','city','postal_city','service_master_pk','last_service_pk','t-Shirt','Blazer','Trouser','Tracksuite','father_profession',
-        'mother_profession'
-    ];
-
-    $isTableField = isset($field->field_type);
-    $fieldType = $isTableField ? $field->field_type : $field->formtype;
-    $fieldLabel = $isTableField ? $field->field_title ?? '' : $field->formlabel ?? '';
-    $fieldName = $name ?? ($isTableField ? "table_{$i}_{$j}" : "field_{$field->formname}");
-    $required = $isTableField ? $field->required ?? false : $field->required ?? false;
-    $requiredAsterisk = $required ? '<span class="text-danger">*</span>' : '';
-    $mappedHeading = '';
-    $isMappedField = false;
-    foreach ($validFieldHeadings as $validHeading) {
-        // Prefer exact match first
-        if (strcasecmp($field->formname ?? ($field->field_title ?? ''), $validHeading) === 0) {
-            $isMappedField = true;
-            $mappedHeading = $validHeading;
-            break;
-        }
-    }
-
-    // If no exact match, fall back to partial match
-    if (!$isMappedField) {
-        foreach ($validFieldHeadings as $validHeading) {
-            if (stripos($field->formname ?? ($field->field_title ?? ''), $validHeading) !== false) {
-                $isMappedField = true;
-                $mappedHeading = $validHeading;
-                break;
-            }
-        }
-    }
-
-@endphp --}}
-
 @php
     $validFieldHeadings = [
-        'country','state','district','language','admissioncategory','stream','institution','jobtype','boardname','qualification',
-        'religion_master_pk','last_service_pk','sports','size','fcscale','distinction','fatherprofession',
-        'shoessize','studentskill','birth_district','birth_state','birth_country','pdistrict_id','mdistrict_id','admission_category_pk',
-        'highest_stream_pk','city','postal_city','service_master_pk','last_service_pk','t-Shirt','Blazer','Trouser','Tracksuite','father_profession',
-        'mother_profession','Degree','instituitontype'
-    ];
+    'country','state','district','language','admissioncategory','stream','institution','jobtype','boardname','qualification',
+    'religionmasterpk','lastservicepk','sports','size','fcscale','distinction','shoessize',
+    'studentskill','birthdistrict','birthstate','birthcountry','pdistrictid','mdistrictid','admissioncategorypk',
+    'higheststreampk','city','postalcity','servicemasterpk','tshirt','blazer','trouser','tracksuite',
+    'fatherprofession','motherprofession','degree','instituitontype'
+];
 
     $isTableField = isset($field->field_type);
     $fieldType = $isTableField ? $field->field_type : $field->formtype;
-
     // Label
-    $fieldLabel = $isTableField ? ($field->field_title ?? $field->header ?? '') : ($field->formlabel ?? '');
-
-    // ✅ Key for mapping (use header if table, else formname/title)
-    // $fieldKey = (trim(
-    //     $isTableField
-    //         ? ($field->header ?? $field->field_title ?? '')
-    //         : ($field->formname ?? $field->field_title ?? '')
-    // ));
-   $fieldKey = strtolower(
-    preg_replace('/[^a-z0-9]/i', '', trim(
-        $isTableField
-            ? ($field->header ?? $field->field_title ?? '')
-            : ($field->formname ?? $field->field_title ?? '')
-    ))
-);
-
+    $fieldLabel = $isTableField ? $field->field_title ?? ($field->header ?? '') : $field->formlabel ?? '';
+    $fieldKey = strtolower(
+        preg_replace(
+            '/[^a-z0-9]/i',
+            '',
+            trim(
+                $isTableField
+                    ? $field->header ?? ($field->field_title ?? '')
+                    : $field->formname ?? ($field->field_title ?? ''),
+            ),
+        ),
+    );
     $fieldName = $name ?? ($isTableField ? "table_{$i}_{$j}" : "field_{$field->formname}");
     $required = $field->required ?? false;
     $requiredAsterisk = $required ? '<span class="text-danger">*</span>' : '';
@@ -134,91 +89,76 @@
         </div>
     @break
 
-  @case('Select Box')
-@case('dropdown')
-    <div class="form-group">
-     <label class="form-label" for="{{ $fieldName }}">{!! $fieldLabel . $requiredAsterisk !!}</label>
-        <select class="form-control select" id="{{ $fieldName }}" name="{{ $fieldName }}"
-            {{ $required ? 'required' : '' }}>
-            <option value="">Choose Option</option>
+    @case('Select Box')
+    @case('dropdown')
+        <div class="form-group">
+            <label class="form-label" for="{{ $fieldName }}">{!! $fieldLabel . $requiredAsterisk !!}</label>
+            <select class="form-control select" id="{{ $fieldName }}" name="{{ $fieldName }}"
+                {{ $required ? 'required' : '' }}>
+                <option value="">Choose Option</option>
 
-            @if ($isMappedField)
-                @php
-                    // Mapping: heading → [table, pk_field, value_field]
-                    $specialMappings = [
-                        'country' => ['country_master', 'pk', 'country_name'],
-                        'postal_country' => ['country_master', 'pk', 'country_name'],
+                @if ($isMappedField)
+                    @php
+                        // Mapping: heading → [table, pk_field, value_field]
+                        $specialMappings = [
+                            'country' => ['country_master', 'pk', 'country_name'],
+                            'state' => ['state_master', 'pk', 'state_name'],
+                            'district' => ['state_district_mapping', 'pk', 'district_name'],
+                            'admissioncategorypk' => ['admission_category_master', 'pk', 'Seat_name'],
+                            'higheststreampk' => ['stream_master', 'pk', 'stream_name'],
+                            'religionmasterpk' => ['religion_master', 'pk', 'religion_name'],
+                            'servicemasterpk' => ['service_master', 'pk', 'service_name'],
+                            'lastservicepk' => ['service_master', 'pk', 'service_name'],
+                            'city' => ['city_master', 'pk', 'city_name'],
+                            'tshirt' => ['student_cloths_size_master', 'pk', 'cloth_size'],
+                            'trouser' => ['student_cloths_size_master', 'pk', 'cloth_size'],
+                            'blazer' => ['student_cloths_size_master', 'pk', 'cloth_size'],
+                            'tracksuite' => ['student_cloths_size_master', 'pk', 'cloth_size'],
+                            'fatherprofession' => ['parents_profession_master', 'pk', 'profession_name'],
+                            'motherprofession' => ['parents_profession_master', 'pk', 'profession_name'],
+                            'language' => ['language_master', 'pk', 'language_name'],
+                            'boardname' => ['university_board_name_master', 'pk', 'board_name'],
+                            'university' => ['university_board_name_master', 'pk', 'board_name'],
+                            'degree' => ['degree_master', 'Pk', 'degree_name'],
+                            'instituitontype' => ['institute_type_master', 'pk', 'type_name'],
+                        ];
 
-                        'state' => ['state_master', 'pk', 'state_name'],
-                        'postal_state' => ['state_master', 'pk', 'state_name'],
-                        'domicile_state' => ['state_master', 'pk', 'state_name'],
-                        'birth_state' => ['state_master', 'pk', 'state_name'],
+                        // $map = $specialMappings[$mappedHeading] ?? null;
+                        $map = $specialMappings[strtolower($mappedHeading)] ?? null;
 
-                        'birth_district' => ['state_district_mapping', 'pk', 'district_name'],
-                        'pdistrict_id' => ['state_district_mapping', 'pk', 'district_name'],
-                        'mdistrict_id' => ['state_district_mapping', 'pk', 'district_name'],
+                        if ($map) {
+                            [$tableName, $pkField, $valueField] = $map;
+                            $options = DB::table($tableName)->get();
+                        } else {
+                            $options = collect(); // empty
+                        }
+                    @endphp
 
-                        'admission_category_pk' => ['admission_category_master', 'pk', 'Seat_name'],
-                        'highest_stream_pk' => ['stream_master', 'pk', 'stream_name'],
+                    @foreach ($options as $option)
+                        <option value="{{ $option->$pkField }}"
+                            {{ old($fieldName, $value) == $option->$pkField ? 'selected' : '' }}>
+                            {{ $option->$valueField }}
+                        </option>
+                    @endforeach
+                @else
+                    @php
+                        $optionsRaw =
+                            $field->field_options ??
+                            ($field->field_checkbox_options ??
+                                ($field->field_radio_options ?? ($field->fieldoption ?? '')));
+                        $options = explode(',', $optionsRaw);
+                    @endphp
 
-                        'religion_master_pk' => ['religion_master', 'pk', 'religion_name'],
-
-                        'service_master_pk' => ['service_master', 'pk', 'service_name'],
-                        'last_service_pk' => ['service_master', 'pk', 'service_name'],
-                        'city' => ['city_master', 'pk', 'city_name'],
-                        'postal_city' => ['city_master', 'pk', 'city_name'],
-                        't-Shirt'      => ['student_cloths_size_master',  'pk', 'cloth_size'],
-                        'Trouser'       => ['student_cloths_size_master',  'pk', 'cloth_size'],
-                        'Blazer'       => ['student_cloths_size_master',  'pk', 'cloth_size'],
-                        'Tracksuite'    => ['student_cloths_size_master',  'pk', 'cloth_size'],
-                        'father_profession' => ['parents_profession_master', 'pk', 'profession_name'],
-                        'mother_profession' => ['parents_profession_master', 'pk', 'profession_name'],
-                        'language' => ['language_master', 'pk', 'language_name'],
-                        'boardname' => ['university_board_name_master', 'pk', 'board_name'],
-                        'university' => ['university_board_name_master', 'pk', 'board_name'],
-                        'degree' => ['degree_master', 'Pk', 'degree_name'],
-                        'instituitontype' => ['institute_type_master', 'pk', 'type_name'],
-                    ];
-
-                    // $map = $specialMappings[$mappedHeading] ?? null;
-                     $map = $specialMappings[strtolower($mappedHeading)] ?? null;
-
-
-                    if ($map) {
-                        [$tableName, $pkField, $valueField] = $map;
-                        $options = DB::table($tableName)->get();
-                    } else {
-                        $options = collect(); // empty
-                    }
-                @endphp
-
-                @foreach ($options as $option)
-                    <option value="{{ $option->$pkField }}"
-                        {{ old($fieldName, $value) == $option->$pkField ? 'selected' : '' }}>
-                        {{ $option->$valueField }}
-                    </option>
-                @endforeach
-            @else
-                @php
-                    $optionsRaw =
-                        $field->field_options ??
-                        ($field->field_checkbox_options ??
-                            ($field->field_radio_options ?? ($field->fieldoption ?? '')));
-                    $options = explode(',', $optionsRaw);
-                @endphp
-
-                @foreach ($options as $option)
-                    @php $option = trim($option); @endphp
-                    <option value="{{ $option }}" {{ old($fieldName, $value) == $option ? 'selected' : '' }}>
-                        {{ $option }}
-                    </option>
-                @endforeach
-            @endif
-        </select>
-    </div>
-@break
-
-
+                    @foreach ($options as $option)
+                        @php $option = trim($option); @endphp
+                        <option value="{{ $option }}" {{ old($fieldName, $value) == $option ? 'selected' : '' }}>
+                            {{ $option }}
+                        </option>
+                    @endforeach
+                @endif
+            </select>
+        </div>
+    @break
 
     @case('Radio Button')
     @case('radio')
@@ -308,4 +248,3 @@
     @default
         <p>Unknown field type: {{ $fieldType }}</p>
 @endswitch
-
