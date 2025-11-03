3758cb30e2f8736623fa2655ecb04fde3648448d
nibhashlbsnaa <nibhash.mishra@awzpact.com>
2025-11-03 17:14:36 +0530

    councillor group

---
+++ resources/views/admin/councillor/create.blade.php
@@ -0,0 +1,10 @@
+@extends('admin.layouts.master')
+
+@section('title', 'Councillor Group - Sargam | Lal Bahadur Shastri National Academy of Administration')
+
+@section('content')
+    <div class="container-fluid">
+
+        <x-breadcrum title="Councillor Group - Create" />
+        <x-session_message />
+@endsection
