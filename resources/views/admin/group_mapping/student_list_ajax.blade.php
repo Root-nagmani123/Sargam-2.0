314b3b049960a7c0bd53227cbce2e6ea5c593119
vinitagupta-tech <vinita.gupta@awzpact.com>
2025-11-06 11:13:43 +0530

    correct mapping

--- resources/views/admin/group_mapping/student_list_ajax.blade.php
+++ resources/views/admin/group_mapping/student_list_ajax.blade.php
@@ -40,6 +40,7 @@
             <th>Display Name</th>
             <th>Email</th>
             <th>Contact No</th>
+            <th>Counsellor Group Name</th>
         </tr>
     </thead>
     <tbody>
