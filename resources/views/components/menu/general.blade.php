b7516d87a0f7e13338d9720be305a8c0b3537184
vinitagupta-tech <vinita.gupta@awzpact.com>
2025-11-11 16:56:36 +0530

    Subject Master & module

--- resources/views/components/menu/general.blade.php
+++ resources/views/components/menu/general.blade.php
@@ -60,14 +60,14 @@
                                         <iconify-icon icon="solar:calendar-mark-line-duotone"></iconify-icon><span
                                             class="hide-menu">Course Group Mapping</span>
                                     </a></li>
+                                <li class="sidebar-item"><a class="sidebar-link" href="{{ route('subject.index') }}">
+                                        <iconify-icon icon="solar:speaker-minimalistic-line-duotone"></iconify-icon>
+                                        <span class="hide-menu">Subject Master</span>
+                                    </a></li>
                                 <li class="sidebar-item"><a class="sidebar-link"
                                         href="{{ route('subject-module.index') }}">
                                         <iconify-icon icon="solar:widget-4-line-duotone"></iconify-icon><span
-                                            class="hide-menu">Subject Module</span>
-                                    </a></li>
-                                <li class="sidebar-item"><a class="sidebar-link" href="{{ route('subject.index') }}">
-                                        <iconify-icon icon="solar:speaker-minimalistic-line-duotone"></iconify-icon>
-                                        <span class="hide-menu">Subject</span>
+                                            class="hide-menu">Subject Module Master</span>
                                     </a></li>
                             </ul>
 
