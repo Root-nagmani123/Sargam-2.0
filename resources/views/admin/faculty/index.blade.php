bdb10311a340ea1a31aee272cb75e032371e4bcc
dhananjaysingh <dhananjaysingh.negd@digitalindia.gov.in>
2025-11-27 14:41:22 +0530

    faculty conflict resolved

--- resources/views/admin/faculty/index.blade.php
+++ resources/views/admin/faculty/index.blade.php
@@ -38,6 +38,9 @@
                                     <span class="material-symbols-rounded fs-5">export_notes</span>
                                     Export Excel
                                 </a>
+                                <a href="{{ route('faculty.printBlank') }}" target="_blank" class="btn btn-success">
+									<i class="material-icons">print</i> Print Blank Form
+								</a>
 
                                 <!-- Search Expand -->
                                 <!-- Search Expand -->
