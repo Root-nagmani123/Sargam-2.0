8045817568ecc99c9661f21ff8a3fce77c8f85ea
nibhashlbsnaa <nibhash.mishra@awzpact.com>
2025-12-11 17:41:49 +0530

    sidebar fixes

--- resources/views/components/menu/setup_academic.blade.php
+++ resources/views/components/menu/setup_academic.blade.php
@@ -1,22 +1,15 @@
-<nav class="sidebar-nav" id="menu-right-setup-mini-4">
-    <div style="display: flex; flex-direction: column; height: 100%;">
-        <!-- Profile (Fixed) -->
-        <div style="padding: 20px 24px; border-bottom: 1px solid rgba(255,255,255,0.1); flex-shrink: 0;">
-            @include('components.profile')
+<nav class="sidebar-nav simplebar-scrollable-y" id="menu-right-setup-mini-4" data-simplebar="init">
+    <div class="simplebar-wrapper" style="margin: 0px -20px -24px;">
+        <div class="simplebar-height-auto-observer-wrapper">
+            <div class="simplebar-height-auto-observer"></div>
         </div>
-        
-        <!-- Menu Items (Scrollable) -->
-        <div class="simplebar-scrollable-y" data-simplebar="init" style="flex: 1; overflow: hidden;">
-            <div class="simplebar-wrapper" style="margin: 0px -20px -24px;">
-                <div class="simplebar-height-auto-observer-wrapper">
-                    <div class="simplebar-height-auto-observer"></div>
-                </div>
-                <div class="simplebar-mask">
-                    <div class="simplebar-offset" style="right: 0px; bottom: 0px;">
-                        <div class="simplebar-content-wrapper" tabindex="0" role="region" aria-label="scrollable content"
-                            style="height: 100%; overflow: hidden scroll;">
-                            <div class="simplebar-content" style="padding: 20px 0px 20px 24px;">
-                                <ul class="sidebar-menu" id="sidebarnav">
+        <div class="simplebar-mask">
+            <div class="simplebar-offset" style="right: 0px; bottom: 0px;">
+                <div class="simplebar-content-wrapper" tabindex="0" role="region" aria-label="scrollable content"
+                    style="height: 100%; overflow: hidden scroll;">
+                    <div class="simplebar-content" style="padding: 20px 0px 20px 24px;">
+                        <ul class="sidebar-menu" id="sidebarnav">
+                            @include('components.profile')
                             <!-- ---------------------------------- -->
                             <!-- Home -->
                             <!-- ---------------------------------- -->
@@ -79,20 +72,20 @@
                                     <i class="material-icons menu-icon material-symbols-rounded"
                                         style="font-size: 24px;">keyboard_arrow_down</i>
                                 </a>
-                                <ul class="collapse list-unstyled ps-3" id="coursemasterCollapse">
-                                    <li class="sidebar-item"><a class="sidebar-link" href="{{ route('programme.index') }}">
-                                            <span class="hide-menu">Course Master</span>
-                                        </a></li>
-                                    <li class="sidebar-item"><a class="sidebar-link"
-                                            href="{{ route('master.course.group.type.index') }}">
-                                            <span class="hide-menu">Course Group Type</span>
-                                        </a></li>
-                                    <li class="sidebar-item"><a class="sidebar-link"
-                                            href="{{ route('group.mapping.index') }}">
-                                            <span class="hide-menu">Course Group Mapping</span>
-                                        </a></li>
-                                </ul>
                             </li>
+                            <ul class="collapse list-unstyled ps-3" id="coursemasterCollapse">
+                                <li class="sidebar-item"><a class="sidebar-link" href="{{ route('programme.index') }}">
+                                        <span class="hide-menu">Course Master</span>
+                                    </a></li>
+                                <li class="sidebar-item"><a class="sidebar-link"
+                                        href="{{ route('master.course.group.type.index') }}">
+                                        <span class="hide-menu">Course Group Type</span>
+                                    </a></li>
+                                <li class="sidebar-item"><a class="sidebar-link"
+                                        href="{{ route('group.mapping.index') }}">
+                                        <span class="hide-menu">Course Group Mapping</span>
+                                    </a></li>
+                            </ul>
                             <li class="sidebar-item" style="background: #4077ad;
                             border-radius: 30px 0px 0px 30px;
                             width: 100%;
@@ -105,40 +98,40 @@
                                     <i class="material-icons menu-icon material-symbols-rounded"
                                         style="font-size: 24px;">keyboard_arrow_down</i>
                                 </a>
-                                <ul class="collapse list-unstyled ps-3" id="exemptionmasterCollapse">
-                                    <li class="sidebar-item"><a class="sidebar-link"
-                                            href="{{ route('student.medical.exemption.index') }}">
-                                            <span class="hide-menu">Student Medical Exemption (Doctor)</span>
-                                        </a></li>
-                                    <li class="sidebar-item"><a class="sidebar-link"
-                                            href="{{ route('mdo-escrot-exemption.index') }}">
-                                            <span class="hide-menu">MDO Escort Exemption</span>
-                                        </a></li>
-                                    <li class="sidebar-item">
-                                        <a class="sidebar-link d-flex justify-content-between align-items-center"
-                                            data-bs-toggle="collapse" href="#exemptionCollapse" role="button"
-                                            aria-expanded="false" aria-controls="exemptionCollapse">
-                                            <span class="hide-menu fw-bold">Exemption Master</span>
-                                            <i class="material-icons menu-icon material-symbols-rounded"
-                                                style="font-size: 24px;">keyboard_arrow_down</i>
-                                        </a>
-                                        <ul class="collapse list-unstyled ps-3" id="exemptionCollapse">
-                                            <li class="sidebar-item"><a class="sidebar-link"
-                                                    href="{{ route('master.exemption.category.master.index') }}">
-                                                    <span class="hide-menu">Exemption Category</span>
-                                                </a></li>
-                                            <li class="sidebar-item"><a class="sidebar-link"
-                                                    href="{{ route('master.exemption.medical.speciality.index') }}">
-                                                    <span class="hide-menu">Exemption Medical Speciality</span>
-                                                </a></li>
-                                            <li class="sidebar-item"><a class="sidebar-link"
-                                                    href="{{ route('master.mdo_duty_type.index') }}">
-                                                    <span class="hide-menu">Duty Type</span>
-                                                </a></li>
-                                        </ul>
-                                    </li>
-                                </ul>
                             </li>
+                            <ul class="collapse list-unstyled ps-3" id="exemptionmasterCollapse">
+                                <li class="sidebar-item"><a class="sidebar-link"
+                                        href="{{ route('student.medical.exemption.index') }}">
+                                        <span class="hide-menu">Student Medical Exemption (Doctor)</span>
+                                    </a></li>
+                                <li class="sidebar-item"><a class="sidebar-link"
+                                        href="{{ route('mdo-escrot-exemption.index') }}">
+                                        <span class="hide-menu">MDO Escort Exemption</span>
+                                    </a></li>
+                                <li class="sidebar-item">
+                                    <a class="sidebar-link d-flex justify-content-between align-items-center"
+                                        data-bs-toggle="collapse" href="#exemptionCollapse" role="button"
+                                        aria-expanded="false" aria-controls="exemptionCollapse">
+                                        <span class="hide-menu fw-bold">Exemption Master</span>
+                                        <i class="material-icons menu-icon material-symbols-rounded"
+                                            style="font-size: 24px;">keyboard_arrow_down</i>
+                                    </a>
+                                </li>
+                                <ul class="collapse list-unstyled ps-3" id="exemptionCollapse">
+                                    <li class="sidebar-item"><a class="sidebar-link"
+                                            href="{{ route('master.exemption.category.master.index') }}">
+                                            <span class="hide-menu">Exemption Category</span>
+                                        </a></li>
+                                    <li class="sidebar-item"><a class="sidebar-link"
+                                            href="{{ route('master.exemption.medical.speciality.index') }}">
+                                            <span class="hide-menu">Exemption Medical Speciality</span>
+                                        </a></li>
+                                    <li class="sidebar-item"><a class="sidebar-link"
+                                            href="{{ route('master.mdo_duty_type.index') }}">
+                                            <span class="hide-menu">Duty Type</span>
+                                        </a></li>
+                                </ul>
+                            </ul>
                             <li class="sidebar-item" style="background: #4077ad;
     border-radius: 30px 0px 0px 30px;
     width: 100%;
@@ -151,25 +144,25 @@
                                     <i class="material-icons menu-icon material-symbols-rounded"
                                         style="font-size: 24px;">keyboard_arrow_down</i>
                                 </a>
-                                <ul class="collapse list-unstyled ps-3" id="memoCollapse">
-                                    <li class="sidebar-item"><a class="sidebar-link"
-                                            href="{{ route('master.memo.type.master.index') }}">
-                                            <span class="hide-menu">Memo Type Master</span>
-                                        </a></li>
-                                    <li class="sidebar-item"><a class="sidebar-link"
-                                            href="{{ route('master.memo.conclusion.master.index') }}">
-                                            <span class="hide-menu">Memo Conclusion Master</span>
-                                        </a></li>
-                                    <li class="sidebar-item"><a class="sidebar-link"
-                                            href="{{ route('course.memo.decision.index') }}">
-                                            <span class="hide-menu">Memo Course Mapping</span>
-                                        </a></li>
-                                    <li class="sidebar-item"><a class="sidebar-link"
-                                            href="{{ route('memo.notice.management.user') }}">
-                                            <span class="hide-menu">Memo & Notice Chat (User)</span>
-                                        </a></li>
-                                </ul>
                             </li>
+                            <ul class="collapse list-unstyled ps-3" id="memoCollapse">
+                                <li class="sidebar-item"><a class="sidebar-link"
+                                        href="{{ route('master.memo.type.master.index') }}">
+                                        <span class="hide-menu">Memo Type Master</span>
+                                    </a></li>
+                                <li class="sidebar-item"><a class="sidebar-link"
+                                        href="{{ route('master.memo.conclusion.master.index') }}">
+                                        <span class="hide-menu">Memo Conclusion Master</span>
+                                    </a></li>
+                                <li class="sidebar-item"><a class="sidebar-link"
+                                        href="{{ route('course.memo.decision.index') }}">
+                                        <span class="hide-menu">Memo Course Mapping</span>
+                                    </a></li>
+                                <li class="sidebar-item"><a class="sidebar-link"
+                                        href="{{ route('memo.notice.management.user') }}">
+                                        <span class="hide-menu">Memo & Notice Chat (User)</span>
+                                    </a></li>
+                            </ul>
                             <li class="sidebar-item" style="background: #4077ad;
     border-radius: 30px 0px 0px 30px;
     width: 100%;
@@ -182,17 +175,17 @@
                                     <i class="material-icons menu-icon material-symbols-rounded"
                                         style="font-size: 24px;">keyboard_arrow_down</i>
                                 </a>
-                                <ul class="collapse list-unstyled ps-3" id="feedbackCollapse">
-                                    <li class="sidebar-item"><a class="sidebar-link"
-                                            href="{{ route('feedback.get.feedbackList') }}">
-                                            <span class="hide-menu">Feedback</span>
-                                        </a></li>
-                                    <li class="sidebar-item"><a class="sidebar-link"
-                                            href="{{ route('feedback.get.studentFeedback') }}">
-                                            <span class="hide-menu">Student Feedback</span>
-                                        </a></li>
-                                </ul>
                             </li>
+                            <ul class="collapse list-unstyled ps-3" id="feedbackCollapse">
+                                <li class="sidebar-item"><a class="sidebar-link"
+                                        href="{{ route('feedback.get.feedbackList') }}">
+                                        <span class="hide-menu">Feedback</span>
+                                    </a></li>
+                                <li class="sidebar-item"><a class="sidebar-link"
+                                        href="{{ route('feedback.get.studentFeedback') }}">
+                                        <span class="hide-menu">Student Feedback</span>
+                                    </a></li>
+                            </ul>
                           @endif
 
                             <!-- faculty menu start -->
@@ -246,11 +239,8 @@
                                     href="{{ route('medical.exception.ot.view') }}">
                                     <span class="hide-menu">Medical Exemption</span>
                                 </a></li>
-                            <li class="sidebar-item"><a class="sidebar-link" href="{{ route('memo.notice.management.user') }}">
-                                    <span class="hide-menu">Memo/Notice View</span>
-                                </a></li>
                             <li class="sidebar-item"><a class="sidebar-link" href="{{ route('ot.notice.memo.view') }}">
-                                    <span class="hide-menu">Memo/Notice Activity</span>
+                                    <span class="hide-menu">Memo/Notice</span>
                                 </a></li>
                             <li class="sidebar-item"><a class="sidebar-link"
                                     href="{{ route('feedback.get.studentFeedback') }}">
@@ -260,20 +250,18 @@
                                     <span class="hide-menu">Peer Evaluation</span>
                                 </a></li>
                             @endif
-                                </ul>
-                            </div>
-                        </div>
+                        </ul>
                     </div>
                 </div>
-                <div class="simplebar-placeholder" style="width: 240px; height: 864px;"></div>
             </div>
-            <div class="simplebar-track simplebar-horizontal" style="visibility: hidden;">
-                <div class="simplebar-scrollbar" style="width: 0px; display: none;"></div>
-            </div>
-            <div class="simplebar-track simplebar-vertical" style="visibility: visible;">
-                <div class="simplebar-scrollbar" style="height: 45px; display: block; transform: translate3d(0px, 0px, 0px);">
-                </div>
-            </div>
+        </div>
+        <div class="simplebar-placeholder" style="width: 240px; height: 864px;"></div>
+    </div>
+    <div class="simplebar-track simplebar-horizontal" style="visibility: hidden;">
+        <div class="simplebar-scrollbar" style="width: 0px; display: none;"></div>
+    </div>
+    <div class="simplebar-track simplebar-vertical" style="visibility: visible;">
+        <div class="simplebar-scrollbar" style="height: 45px; display: block; transform: translate3d(0px, 0px, 0px);">
         </div>
     </div>
 </nav>
