d5b71474b946d3e6a2bcd281bdf5c1ac9eb9ef66
vinitagupta-tech <vinita.gupta@awzpact.com>
2025-12-17 16:34:27 +0530

    notification improve & course -issue

--- app/Http/Controllers/Admin/CourseController.php
+++ app/Http/Controllers/Admin/CourseController.php
@@ -302,7 +302,9 @@ class CourseController extends Controller
             // return redirect()->back()->with('error', $e->getMessage());
             return redirect()->back()->with('error', 'Something went wrong. Please try again.');
         }
-    }
+    } 
+
+
 
     public function view($id)
     {
