157a68b4e0107765384e3f07e7ad535960a0c7e6
nibhashlbsnaa <nibhash.mishra@awzpact.com>
2025-12-19 19:22:55 +0530

    datatable in active

--- resources/views/admin/dashboard/incoming_course.blade.php
+++ resources/views/admin/dashboard/incoming_course.blade.php
@@ -10,30 +10,37 @@
         <div class="card-body">
             <h4>Incoming Course</h4>
             <hr class="my-2">
-            <div class="table-responsive">
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">Course Name</th>
-                            <th scope="col">Short Name</th>
-                            <th scope="col">Start Date</th>
-                            <th scope="col">End Date</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($active_courses as $course)
-                        <tr>
-                            <td>{{ $course->course_name }}</td>
-                            <td>{{ $course->couse_short_name }}</td>
-                            <td>{{ \Carbon\Carbon::parse($course->start_year)->format('d M Y') }}</td>
-                            <td>{{ \Carbon\Carbon::parse($course->end_date)->format('d M Y') }}</td>
-                        </tr>
-                        @endforeach
+            <div class="datatables">
+                <div class="table-responsive">
+                    <table class="table" id="dom_jq_event">
+                        <thead>
+                            <tr>
+                                <th scope="col">Sl. No.</th>
+                                <th scope="col">Course Name</th>
+                                <th scope="col">Short Name</th>
+                                <th scope="col">Start Date</th>
+                                <th scope="col">End Date</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($incoming_courses as $course)
+                            <tr>
+                                <td>{{ $loop->iteration }}</td>
+                                <td>{{ $course->course_name }}</td>
+                                <td>{{ $course->couse_short_name }}</td>
+                                <td>{{ \Carbon\Carbon::parse($course->start_year)->format('d M Y') }}</td>
+                                <td>{{ \Carbon\Carbon::parse($course->end_date)->format('d M Y') }}</td>
+                                <td><a href="{{ route('programme.show', encrypt($course->pk)) }}"
+                                        class="btn btn-sm btn-primary">View Details</a></td>
+                            </tr>
+                            @endforeach
 
 
 
-                    </tbody>
-                </table>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
