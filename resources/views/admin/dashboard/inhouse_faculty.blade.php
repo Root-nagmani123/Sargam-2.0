2767398c5fabcfcd7017012fce7fe43eaaf380d9
Root-nagmani123 <nagmani.singh@awzpact.com>
2025-12-19 18:53:09 +0530

    some bug fix given by prerna

--- resources/views/admin/dashboard/inhouse_faculty.blade.php
+++ resources/views/admin/dashboard/inhouse_faculty.blade.php
@@ -10,16 +10,26 @@
             <h4>Inhouse Faculty</h4>
             <hr class="my-2">
             <div class="table-responsive">
-                <table class="table">
+               <table class="table">
                     <thead>
                         <tr>
-                            <th scope="col">Course Name</th>
-                            <th scope="col">Instructor</th>
-                            <th scope="col">Start Date</th>
-                            <th scope="col">End Date</th>
+                            <th scope="col">Sl. No.</th>
+                            <th scope="col">Faculty Type</th>
+                            <th scope="col">Faculty Name</th>
+                            <th scope="col">Mobile Number</th>
+                            <th scope="col">Current Sector</th>
                         </tr>
                     </thead>
                     <tbody>
+                        @foreach($inhouse_faculty as $index => $faculty)
+                        <tr>
+                            <td>{{ $index + 1 }}</td>
+                            <td class="  text-center"><span class="badge bg-success-subtle text-success">Inhouse</span></td>
+                            <td>{{ $faculty->full_name }}</td>
+                            <td>{{ $faculty->mobile_no }}</td>
+                            <td class="  text-center"><span class="badge bg-success-subtle text-success">@if($faculty->faculty_sector == 1){{ 'Government' }}@elseif($faculty->faculty_sector == 2){{ 'Private' }}@else{{ 'Other' }}@endif</span></td>
+                             </tr>
+                        @endforeach
 
 
 
