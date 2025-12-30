ed91a64d40062ab975c0b5619ac0132e5f28fc3d
nibhashlbsnaa <nibhash.mishra@awzpact.com>
2025-12-30 14:35:28 +0530

    upcoming events

--- resources/views/fc/layouts/header.blade.php
+++ resources/views/fc/layouts/header.blade.php
@@ -1,7 +1,7 @@
-   <!-- Top Blue Bar (Govt of India) -->
-   <div class="top-header d-flex justify-content-between align-items-center d-none d-md-block">
+   <!-- Top Blue Bar (Govt of India) - Hidden on mobile -->
+   <div class="top-header d-none d-md-block">
        <div class="container">
-           <div class="row">
+           <div class="row align-items-center">
                <div class="col-md-3 d-flex align-items-center">
                    <img src="https://upload.wikimedia.org/wikipedia/en/thumb/4/41/Flag_of_India.svg/800px-Flag_of_India.svg.png"
                        alt="GoI Logo" height="30">
@@ -51,32 +51,38 @@
    <!-- Sticky Header -->
    <div class="header sticky-top bg-white shadow-sm">
        <div class="container">
-           <nav class="navbar navbar-expand-lg">
+           <nav class="navbar navbar-expand-lg navbar-light">
                <div class="container-fluid px-0">
-                   <a class="navbar-brand me-2" href="#">
+                   <!-- Logo 1 -->
+                   <a class="navbar-brand me-1 me-md-2" href="#">
                        <img src="https://i.pinimg.com/736x/a8/fa/ef/a8faef978e6230b6a12d1c29c62d5edf.jpg" alt="Logo 1"
-                           height="80">
+                           class="img-fluid" height="80">
                    </a>
-                   <span class="vr mx-2"></span>
-                   <a class="navbar-brand" href="#">
-                       <img src="https://www.lbsnaa.gov.in/admin_assets/images/logo.png" alt="Logo 2" height="80">
+                   <!-- Divider - Hidden on mobile -->
+                   <span class="vr mx-1 mx-md-2 d-none d-sm-block"></span>
+                   <!-- Logo 2 -->
+                   <a class="navbar-brand me-auto" href="#">
+                       <img src="https://www.lbsnaa.gov.in/admin_assets/images/logo.png" alt="Logo 2" class="img-fluid" height="80">
                    </a>
 
-                   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                   <!-- Mobile toggle button -->
+                   <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                        <span class="navbar-toggler-icon"></span>
                    </button>
 
-                   <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-                       <ul class="navbar-nav align-items-center">
+                   <!-- Navigation Menu -->
+                   <!-- Navigation Menu -->
+                   <div class="collapse navbar-collapse" id="navbarNav">
+                       <ul class="navbar-nav ms-auto align-items-lg-center">
                            <li class="nav-item">
-                               <a class="nav-link ms-4 me-4" href="https://www.lbsnaa.gov.in/menu/about-lbsnaa" target="_blank">About Us</a>
+                               <a class="nav-link fw-medium" href="https://www.lbsnaa.gov.in/menu/about-lbsnaa" target="_blank">About Us</a>
                            </li>
                            <li class="nav-item">
-                               <a class="nav-link ms-4 me-4" href="https://www.lbsnaa.gov.in/footer_menu/contact-us" target="_blank">Contact</a>
+                               <a class="nav-link fw-medium" href="https://www.lbsnaa.gov.in/footer_menu/contact-us" target="_blank">Contact</a>
                            </li>
-                           <li class="nav-item">
-                               <a class="btn btn-outline-primary ms-4" href="{{ route('fc.login') }}">Login</a>
+                           <li class="nav-item mt-2 mt-lg-0">
+                               <a class="btn btn-outline-primary" href="{{ route('fc.login') }}">Login</a>
                            </li>
                        </ul>
                    </div>
