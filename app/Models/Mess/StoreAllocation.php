7e51969bb12d63a951bc9e3d11ac320c7b2eab11
VinitaGupt <vinita.gupta@awzpact.in>
2026-01-30 17:49:21 +0530

    store-allocation

--- app/Models/Mess/StoreAllocation.php
+++ app/Models/Mess/StoreAllocation.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Models\Mess;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -7,6 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class StoreAllocation extends Model
 {
     use HasFactory;
+
     protected $table = 'mess_store_allocations';
-    protected $fillable = ['store_name', 'allocated_to', 'quantity', 'allocation_date'];
+
+    protected $fillable = [
+        'sub_store_id',
+        'allocation_date',
+        'store_name',
+        'allocated_to',
+        'quantity',
+    ];
+
+    protected $casts = [
+        'allocation_date' => 'date',
+    ];
+
+    public function subStore()
+    {
+        return $this->belongsTo(SubStore::class, 'sub_store_id');
+    }
+
+    public function items()
+    {
+        return $this->hasMany(StoreAllocationItem::class, 'store_allocation_id');
+    }
 }
