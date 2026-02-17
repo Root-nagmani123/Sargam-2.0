887679ae26b58c1ef725d70a582bfde553971f40
nibhashlbsnaa <nibhash.mishra@awzpact.com>
2026-02-17 23:59:52 +0530

    estate master

---
+++ app/Models/EligibilityCriterion.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class EligibilityCriterion extends Model
+{
+    protected $table = 'estate_eligibility_mapping';
+
+    protected $primaryKey = 'pk';
+
+    public $timestamps = false;
+
+    protected $fillable = ['pay_scale_master_pk', 'estate_unit_type_master_pk', 'estate_unit_sub_type_master_pk'];
+
+    public function payScale(): BelongsTo
+    {
+        return $this->belongsTo(PayScale::class, 'pay_scale_master_pk', 'pk');
+    }
+
+    public function unitType(): BelongsTo
+    {
+        return $this->belongsTo(UnitType::class, 'estate_unit_type_master_pk', 'pk');
+    }
+
+    public function unitSubType(): BelongsTo
+    {
+        return $this->belongsTo(UnitSubType::class, 'estate_unit_sub_type_master_pk', 'pk');
+    }
+}
