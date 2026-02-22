99b0e606a695ca5a670fca7e75a632f48e7e123d
nibhashlbsnaa <nibhash.mishra@awzpact.com>
2026-02-22 12:38:54 +0530

    estate report

--- routes/web.php
+++ routes/web.php
@@ -1076,7 +1076,13 @@ Route::get('/course-repository-user/{pk}', [CourseRepositoryController::class, '
             Route::get('bill-report-print', [EstateController::class, 'estateBillReportPrint'])->name('bill-report-print');
             Route::get('bill-report-print-all', [EstateController::class, 'estateBillReportPrintAll'])->name('bill-report-print-all');
             Route::get('bill-report-print-all-pdf', [EstateController::class, 'estateBillReportPrintAllPdf'])->name('bill-report-print-all-pdf');
+<<<<<<< HEAD
 >>>>>>> 4140ca09 (estate master and hac)
+=======
+
+            Route::get('migration-report', [EstateController::class, 'estateMigrationReport'])->name('migration-report');
+            Route::get('migration-report/filter-options', [EstateController::class, 'getEstateMigrationReportFilterOptions'])->name('migration-report.filter-options');
+>>>>>>> 8c3b8afa (estate report)
         });
     });
 });
