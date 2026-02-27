e40a8d435c50b69314617142c315d22da5090c26
nibhashlbsnaa <nibhash.mishra@awzpact.com>
2026-02-27 20:50:30 +0530

    timetable

--- resources/views/admin/layouts/timetable.blade.php
+++ resources/views/admin/layouts/timetable.blade.php
@@ -141,8 +141,8 @@
         </div>
     </footer>
 
-    <!-- Bootstrap JS (local) -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
     <!-- JavaScript for Tab Functionality -->
     <script>
