b3b3b38b457988db0bd59e721d2ed65148c363d2
nibhashlbsnaa <nibhash.mishra@awzpact.com>
2026-02-27 20:22:52 +0530

    setup general

--- resources/views/components/menu/setup_general.blade.php
+++ resources/views/components/menu/setup_general.blade.php
@@ -115,6 +115,7 @@
                                 </ul>
                             </li>
                             @endif
+                            @endif
                         
 
                         </ul>
