3be1356bec923491b8050bb7607b43ec807adc6a
nibhashlbsnaa <nibhash.mishra@awzpact.com>
2026-03-03 14:22:52 +0530

    Working on estate module

---
+++ app/Models/EstateMonthReadingDetails.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EstateMonthReadingDetails extends Model
+{
+    protected $table = 'estate_month_reading_details';
+
+    protected $primaryKey = 'pk';
+
+    public $incrementing = true;
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'estate_possession_details_pk',
+        'from_date',
+        'to_date',
+        'last_month_elec_red',
+        'curr_month_elec_red',
+        'bill_month',
+        'bill_year',
+        'house_no',
+        'meter_one',
+        'meter_two',
+    ];
+
+    protected $casts = [
+        'from_date' => 'date',
+        'to_date' => 'date',
+    ];
+
+}
