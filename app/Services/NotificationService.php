615d49dd95a22f347c21fe41b49201b109d56b55
VinitaGupt <vinita.gupta@awzpact.in>
2026-03-24 12:58:05 +0530

    dd-retun_url

--- app/Services/NotificationService.php
+++ app/Services/NotificationService.php
@@ -515,6 +515,7 @@ class NotificationService
         $marked = $this->markAsRead($notificationPk, $userId);
         // print_r($marked);die;
         $redirectUrl = $this->getRedirectUrl($notificationPk);
+        print_r($redirectUrl);die;
         // print_r($redirectUrl);die;
 
         return [
