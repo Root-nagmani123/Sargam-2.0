0691bc02275a5cc64c3d5f378f850064735b2810
hardeeplbsnaa <hardeep.dagar@awzpact.com>
2026-04-22 15:39:18 +0530

    fc migrations

--- database/migrations/FC/2024_01_01_000001_create_jbp_users_table.php
+++ database/migrations/FC/2024_01_01_000001_create_jbp_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Support\MigrationSchema;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -8,7 +9,7 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('jbp_users', function (Blueprint $table) {
+        MigrationSchema::createIfMissing('jbp_users', function (Blueprint $table) {
             $table->id();
             $table->string('username', 100)->unique();
             $table->string('password', 255);
