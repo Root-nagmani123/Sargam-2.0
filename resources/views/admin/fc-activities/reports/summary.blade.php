a3b5c0de0aaf70f985b39f07fb54cf6ccc0eb82e
hardeeplbsnaa <hardeep.dagar@awzpact.com>
2026-04-27 16:40:34 +0530

    activites modules export functionality implemneted

--- resources/views/admin/fc-activities/reports/summary.blade.php
+++ resources/views/admin/fc-activities/reports/summary.blade.php
@@ -2,9 +2,10 @@
 @section('title', 'Activity Reports Summary')
 @section('setup_content')
 <div class="container-fluid px-3">
+    <x-breadcrum title="FC Activities - Report Summary"></x-breadcrum>
     <h4 class="fw-bold mb-3" style="color:#1a3c6e;">Department Report Summary</h4>
     <div class="card border-0 shadow-sm"><div class="table-responsive">
-        <table class="table table-sm table-hover mb-0">
+        <table class="table table-sm table-hover mb-0 js-fc-datatable" data-export-title="FC Activities - Department Summary">
             <thead><tr><th>Admin</th><th>Security</th><th>IT</th><th>Training</th><th>Medical</th><th>Shop</th></tr></thead>
             <tbody><tr>
                 @foreach(['admin','security','it','trg','medical','shop'] as $dept)
@@ -15,3 +16,5 @@
     </div></div>
 </div>
 @endsection
+
+@include('admin.fc-activities.partials.datatable-tools')
