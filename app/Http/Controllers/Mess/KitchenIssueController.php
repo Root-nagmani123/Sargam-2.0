b32bb3f29dd774c13e51285b59e06267ab9c9a57
VinitaGupt <vinita.gupta@awzpact.in>
2026-05-21 14:36:09 +0530

    cascade-issue

--- app/Http/Controllers/Mess/KitchenIssueController.php
+++ app/Http/Controllers/Mess/KitchenIssueController.php
@@ -1526,6 +1526,7 @@ class KitchenIssueController extends Controller
         $kitchenIssue = KitchenIssueMaster::findOrFail($id);
 
         try {
+            $kitchenIssue->items()->delete();
             $kitchenIssue->delete();
 
             return redirect()->route('admin.mess.material-management.index')
