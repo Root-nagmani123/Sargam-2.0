20b8e55b410161139ccc00ac9e4940e222d32ed8
nibhashlbsnaa <nibhash.mishra@awzpact.com>
2026-07-07 09:49:08 +0530

    faculty

--- resources/views/admin/faculty/index.blade.php
+++ resources/views/admin/faculty/index.blade.php
@@ -36,11 +36,39 @@
         <!-- start Zero Configuration -->
         <div class="card" style="border-left:4px solid #004a93;">
             <div class="card-body">
-                <div class="row">
-                    <div class="col-6">
-                        <h4 class="fw-semibold text-primary mb-0" style="color:#004a93 !important;">
-                            Faculty
-                        </h4>
+                <div class="table-responsive">
+                    <div class="row">
+                        <div class="col-6">
+                            <h4 class="fw-semibold text-primary mb-0" style="color:#004a93 !important;">
+                                Faculty
+                            </h4>
+                        </div>
+
+                        <div class="col-6">
+                            <div class="d-flex justify-content-end align-items-center gap-3">
+
+                                <!-- Add Faculty -->
+                                <a href="{{ route('faculty.create') }}"
+                                    class="btn btn-primary d-flex align-items-center gap-1 shadow-sm"
+                                    style="background-color:#004a93; border-color:#004a93;"
+                                    aria-label="Add New Faculty">
+                                    <span class="material-symbols-rounded fs-5">add</span>
+                                    Add Faculty
+                                </a>
+
+                                <!-- Export Excel -->
+                                <a href="{{ route('faculty.excel.export') }}"
+                                    class="btn btn-outline-primary btn-faculty-export d-flex align-items-center gap-1 shadow-sm"
+                                    aria-label="Export Faculty Excel">
+                                    <span class="material-symbols-rounded fs-5">export_notes</span>
+                                    Export Excel
+                                </a>
+                                <a href="{{ route('faculty.printBlank') }}"  class="btn btn-outline-success">
+									<i class="material-icons">print</i> Print Blank Form
+								</a>
+
+                            </div>
+                        </div>
                     </div>
 
                     <hr>
